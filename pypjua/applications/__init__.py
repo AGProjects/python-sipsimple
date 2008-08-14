912f4b0fa95be41ba054ed878140f1a7e86782fa
Luci Stanescu <luci@ag-projects.com>
2008-08-14 10:19:10 +0000

    Redefine XML applications API

--- pypjua/applications/__init__.py
+++ pypjua/applications/__init__.py
@@ -6,78 +6,136 @@ _schema_dir_ = os.path.join(os.path.dirname(__file__), 'xml-schemas')
 
 
 class ParserError(Exception): pass
+class BuilderError(Exception): pass
 
 
-class Parser(object):
-    accept_types = []
-
-class XMLElementMapping(object):
-    _xml_attrs = {}
-    def __init__(self, element, *args, **kwargs):
-        for attr, definition in self._xml_attrs.items():
-            setattr(self, attr, element.get(definition.get('xml_attribute', attr)))
-        self._parse_element(element, *args, **kwargs)
+class XMLMixin(object):
+class XMLElement(object):
+    encoding = 'UTF-8'
     
+    _xml_tag = None
+    _xml_namespace = None
+    _xml_attrs = {}
+
+    @classmethod
+    def tag(cls, namespace = None, tag = None):
+        tag =  tag or cls._xml_tag # define in subclass
+        namespace = namespace or cls._xml_namespace # define in subclass
+        return '{%s}%s' % (namespace, tag)
+
+    def to_element(self, parent = None, namespace = None, tag = None, nsmap = None):
+        if parent is None:
+            element = etree.Element(self.tag(namespace, tag), nsmap = nsmap)
+        else:
+            element = etree.SubElement(parent, self.tag(namespace, tag), nsmap = nsmap)
+        self.build_element(element) # define in subclass
+        return element
+    
+    # To be defined in subclass
+    def _build_element(self, element):
+        pass
+
+    @classmethod
+    def from_element(cls, element, *args, **kwargs):
+        obj = cls.__new__(cls)
+        for attr, definition in cls._xml_attrs.items():
+            setattr(obj, attr, element.get(definition.get('xml_attribute', attr)))
+        obj._parse_element(element, *args, **kwargs)
+        return obj
+    
+    # To be defined in subclass
+    def _parse_element(self, element, *args, **kwargs):
+        pass
+
+    def toxml(self, *args, **kwargs):
+        """Shortcut that generates Element and calls etree.tostring"""
+        element = self.to_element()
+        kwargs.setdefault('encoding', self.encoding)
+        return etree.tostring(element, *args, **kwargs)
+
     def __eq__(self, obj):
         ids = [getattr(self, attr) for attr, definition in self._xml_attrs.items() if definition.get('id_attribute', False)]
         if isinstance(obj, basestring):
             if len(ids) != 1 or ids[0] != obj:
                 return False
         elif isinstance(obj, (tuple, list)):
-            if ids != list(obj):
+            if ids != list(obj) and ids != []:
                 return False
         else:
+            has_test = False
             for attr, definition in self._xml_attrs.items():
-                if definition.get('testequal', True) and (not hasattr(obj, attr) or getattr(self, attr) != getattr(obj, attr)):
-                    return False
+                if definition.get('test_equal', True) or definition.get('id_attribute', False):
+                    if not hasattr(obj, attr) or getattr(self, attr) != getattr(obj, attr):
+                        return False
+                    has_test = True
+            if not has_test:
+                try:
+                    return super(XMLElementMapping, self).__eq__(obj)
+                except AttributeError:
+                    return self is obj
         return True
 
     def __hash__(self):
-        return sum(hash(getattr(self, attr)) for attr, definition in self._xml_attrs.items() if definition.get('id_attribute', False))
-    
-    def _parse_element(self, element, *args, **kwargs):
-        pass
+        id_hashes = [hash(getattr(self, attr)) for attr, definition in self._xml_attrs.items() if definition.get('id_attribute', False)]
+        if len(id_hashes) == 0:
+            return super(XMLElementMapping, self).__hash__()
+        return sum(id_hashes)
 
-def parse_schema(filename, dir = _schema_dir_):
-    return etree.XMLSchema(etree.parse(open(os.path.join(dir, filename), 'r')))
-
-class XMLParserMeta(type):
+class XMLApplicationMeta(type):
     def __init__(cls, name, bases, dct):
-        if cls._schema is None and cls._schema_file is not None:
-            cls._schema = parse_schema(cls._schema_file, cls._schema_dir)
+        if cls._xml_schema is None and cls._xml_schema_file is not None:
+            cls._xml_schema = etree.XMLSchema(etree.parse(open(os.path.join(cls._xml_schema_dir, cls._xml_schema_file), 'r')))
+        if cls._parser is None:
+            if cls._xml_schema is not None and cls._validate_input:
+                cls._parser = etree.XMLParser(schema=cls._xml_schema, **cls._parser_opts)
+            else:
+                cls._parser = etree.XMLParser(**cls._parser_opts)
 
-class XMLParser(Parser):
-    __metaclass__ = XMLParserMeta
+class XMLApplication(XMLElement):
+    __metaclass__ = XMLApplicationMeta
+    
+    encoding = 'UTF-8'
+    accept_types = []
+    build_types = []
+    
+    _validate_input = True
+    _validate_output = True
+    _parser_opts = {}
+    _parser = None
+    
+    _xml_nsmap = {}
+    _xml_schema = None
+    _xml_schema_file = None
+    _xml_schema_dir = _schema_dir_
+    _xml_declaration = False
+    
+    # To be defined in subclasses
+    @classmethod
+    def from_element(cls, element):
+        raise NotImplementedError("from_element method not defined in class %s" % cls.__name__)
 
-    _namespace = None
-    _schema = None
-    _schema_file = None
-    _parser = etree.XMLParser()
+    def to_element(self, parent=None, namespace=None, tag=None, nsmap=None):
+        nsmap = nsmap or  self._xml_nsmap
+        return XMLMixin.to_element(self, parent, namespace, tag, nsmap)
 
-    _schema_dir = _schema_dir_
-
-    def assertValid(self, element):
-        if self._schema is not None:
-            self._schema.assertValid(element)
-
-    def _parse(self, document):
-        if self._parser is not None:
-            try:
-                root = etree.XML(document, self._parser)
-                self.assertValid(root)
-            except Exception, e:
-                raise ParserError("Cannot load XML document: %s" % e)
-            return root
-        else:
-            raise ParserError("No XML parser defined")
+    @classmethod
+    def parse(cls, document):
+        return cls.from_element(etree.XML(document, cls._parser))
+    
+    def toxml(self, *args, **kwargs):
+        element = self.to_element()
+        kwargs.setdefault('encoding', self.encoding)
+        kwargs.setdefault('xml_declaration', self._xml_declaration)
+        validate = kwargs.pop('validate', self._validate_output)
+        res = etree.tostring(element, *args, **kwargs)
+        if validate:
+            self._xml_schema.assertValid(element)
+        return res
 
     def _check_qname(self, element, name, namespace=None):
-        namespace = namespace or self._namespace
-        try:
-            if namespace is not None:
-                name = '{%s}%s' % (namespace, name)
-            if element.tag != name:
-                raise ParserError("Wrong XML element in XML parser %s: expected %s, got %s" %
-                        (self.__class__.__name__, name, element.tag))
-        except KeyError:
-            raise ParserError("Cannot find namespace for XML element %s:%s" % (element.prefix, element.tag))
+        namespace = namespace or self._xml_namespace
+        if namespace is not None:
+            name = '{%s}%s' % (namespace, name)
+        if element.tag != name:
+            raise ParserError("Wrong XML element in XML application %s: expected %s, got %s" %
+                    (self.__class__.__name__, name, element.tag))
