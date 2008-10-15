546adb544585358f6a9e5aad0d086f5b294daa7c
Luci Stanescu <luci@ag-projects.com>
2008-10-15 13:57:21 +0000

    Fix parameter referenced by name rather than via kwargs

--- scripts/sip_subscribe_presence.py
+++ scripts/sip_subscribe_presence.py
@@ -379,7 +379,7 @@ def do_subscribe(**kwargs):
         return
     initial_events = Engine.init_options_defaults["initial_events"]
     if kwargs['content_type'] is not None:
-        initial_events['presence'] = [content_type]
+        initial_events['presence'] = [kwargs['content_type']]
 
     e = Engine(event_handler, do_siptrace=kwargs['do_siptrace'], auto_sound=False, initial_events=initial_events, local_ip=kwargs.pop("local_ip"), local_port=kwargs.pop("local_port"))
     e.start()
