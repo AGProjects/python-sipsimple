fffd6b70312ca4fd4a02447cdf506e92a833558f
Denis Bilenko <denis@ag-projects.com>
2008-11-06 12:36:19 +0000

    added utilities for handling console input in twisted and eventlet

---
+++ pypjua/clients/console.py
@@ -0,0 +1,104 @@
+import tty
+import sys
+import termios
+from twisted.internet import reactor, stdio
+from twisted.conch.insults import insults
+from twisted.conch import recvline
+
+CTRL_C = '\x03'
+CTRL_D = '\x04'
+CTRL_BACKSLASH = '\x1c'
+CTRL_L = '\x0c'
+
+class Console(recvline.HistoricRecvLine):
+    #Copied from twisted.conch.manhole.Manhole but removed interpreter-related stuff
+
+    def initializeScreen(self):
+        self.terminal.write(self.ps[self.pn])
+        self.setInsertMode()
+
+    def connectionMade(self):
+        super(Console, self).connectionMade()
+        self.keyHandlers[CTRL_C] = self.handle_INT
+        self.keyHandlers[CTRL_D] = self.handle_EOF
+        self.keyHandlers[CTRL_L] = self.handle_FF
+        self.keyHandlers[CTRL_BACKSLASH] = self.handle_QUIT
+
+    def handle_INT(self):
+        self.pn = 0
+        self.lineBuffer = []
+        self.lineBufferIndex = 0
+
+        self.terminal.nextLine()
+        self.terminal.write("KeyboardInterrupt")
+        self.terminal.nextLine()
+        self.terminal.write(self.ps[self.pn])
+
+    def handle_EOF(self):
+        #print 'EOF', `self.lineBuffer`
+        self.terminal.loseConnection()
+        #if self.lineBuffer:
+            #self.terminal.write('\a')
+
+    def handle_FF(self):
+        self.terminal.eraseDisplay()
+        self.terminal.cursorHome()
+        self.drawInputLine()
+
+    def handle_QUIT(self):
+        self.terminal.loseConnection()
+
+    def _needsNewline(self):
+        w = self.terminal.lastWrite
+        return not w.endswith('\n') and not w.endswith('\x1bE')
+
+    def addOutput(self, bytes, async=False):
+        if async:
+            self.terminal.eraseLine()
+            self.terminal.cursorBackward(len(self.lineBuffer) + len(self.ps[self.pn]))
+
+        self.terminal.write(bytes)
+
+        if async:
+            if self._needsNewline():
+                self.terminal.nextLine()
+
+            self.terminal.write(self.ps[self.pn])
+
+            if self.lineBuffer:
+                oldBuffer = self.lineBuffer
+                self.lineBuffer = []
+                self.lineBufferIndex = 0
+
+                self._deliverBuffer(oldBuffer)
+
+    def lineReceived(self, line):
+        #more = self.interpreter.push(line)
+        self.pn = 0 # bool(more)
+        if self._needsNewline():
+            self.terminal.nextLine()
+        self.terminal.write(self.ps[self.pn])
+
+class ServerProtocol(insults.ServerProtocol):
+
+    def reset(self):
+        self.cursorPos.x = self.cursorPos.y = 0
+        try:
+            del self._savedCursorPos
+        except AttributeError:
+            pass
+        self.write('\n')
+
+
+if __name__ == '__main__':
+    #log.startLogging(file('child.log', 'w'))
+    fd = sys.__stdin__.fileno()
+    oldSettings = termios.tcgetattr(fd)
+    tty.setraw(fd)
+    try:
+        p = ServerProtocol(Console)
+        stdio.StandardIO(p)
+        reactor.run()
+    finally:
+        termios.tcsetattr(fd, termios.TCSANOW, oldSettings)
+        #os.write(fd, "\r\x1bc\r")
---
+++ pypjua/clients/consolebuffer.py
@@ -0,0 +1,97 @@
+import sys
+import os
+import termios
+import tty
+from twisted.internet.error import ConnectionDone
+from twisted.internet import stdio
+from pypjua.clients.console import Console, ServerProtocol, CTRL_C, CTRL_D, CTRL_BACKSLASH
+from eventlet.channel import channel as Channel
+from eventlet.api import spawn
+
+class _Console(Console):
+
+    channel = Channel()
+    send_keys = [CTRL_C, CTRL_D, CTRL_BACKSLASH]
+
+    def keystrokeReceived(self, keyID, modifier):
+        Console.keystrokeReceived(self, keyID, modifier)
+        if keyID in self.send_keys:
+            spawn(self.channel.send, ('key', keyID))
+
+    def lineReceived(self, line):
+        Console.lineReceived(self, line)
+        spawn(self.channel.send, ('line', line))
+
+    def connectionLost(self, reason):
+        spawn(self.channel.send_exception, reason.value)
+
+class ConsoleBuffer:
+
+    def recv(self):
+        res = self.channel.receive()
+        return res
+
+    def write(self, msg):
+        if self.protocol.terminalProtocol:
+            self.protocol.terminalProtocol.addOutput(msg, async=True)
+        else:
+            __original_sys_stderr__.write('\r%s\n' % msg)
+
+    def write_(self, msg):
+        if msg.endswith('\n'):
+            msg = msg[:-1]
+        return self.write(msg)
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        try:
+            return self.recv()
+        except ConnectionDone:
+            raise StopIteration
+
+def get_console():
+    buffer = ConsoleBuffer()
+    buffer.channel = _Console.channel
+    p = ServerProtocol(_Console)
+    stdio.StandardIO(p)
+    buffer.protocol = p
+    return buffer
+
+__original_sys_stderr__ = sys.stderr
+__original_sys_stdout__ = sys.stdout
+
+class FileProxy(object):
+
+    def __init__(self, original, new_write):
+        self.original = original
+        self.write = new_write
+
+    def __getattr__(self, item):
+        return getattr(self.original, item)
+
+def hook_std_output(console):
+    sys.stdout = FileProxy(sys.stdout, console.write_)
+    sys.stderr = FileProxy(sys.stderr, console.write_)
+
+def restore_std_output():
+    sys.stdout = __original_sys_stdout__
+    sys.stderr = __original_sys_stderr__
+
+if __name__ == '__main__':
+    from twisted.internet import reactor
+    reactor.callLater(2, lambda : sys.stdout.write('Async output\n-------\n'))
+    reactor.callLater(4, lambda : 1/0)
+    fd = sys.__stdin__.fileno()
+    oldSettings = termios.tcgetattr(fd)
+    tty.setraw(fd)
+    try:
+        console = get_console()
+        hook_std_output(console)
+        for type, value in console:
+            console.write('%s %r' % (type, value))
+    finally:
+        restore_std_output()
+        termios.tcsetattr(fd, termios.TCSANOW, oldSettings)
+        os.write(fd, "\r")
