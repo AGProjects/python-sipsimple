522749853473413004dfc186944f37fbbd7f6048
Ruud Klaver <ruud@ag-projects.com>
2008-12-12 15:23:10 +0000

    Ported multi-transport, new route lookup and SIP URI parsing functions, and SIP trace file logging to sip_message.py

--- scripts/sip_message.py
+++ scripts/sip_message.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 import sys
-import re
 import traceback
 import os
 import signal
@@ -9,39 +8,32 @@ import random
 from thread import start_new_thread, allocate_lock
 from Queue import Queue
 from optparse import OptionParser, OptionValueError
-import dns.resolver
 from application.configuration import *
 from application.process import process
 from pypjua import *
 from pypjua.clients import enrollment
+from pypjua.clients.lookup import *
+from pypjua.clients import parse_cmdline_uri
+from pypjua.clients.log import Logger
 
 class GeneralConfig(ConfigSection):
-    _datatypes = {"listen_udp": datatypes.NetworkAddress, "trace_pjsip": datatypes.Boolean, "trace_sip": datatypes.Boolean}
-    listen_udp = datatypes.NetworkAddress("any")
+    _datatypes = {"local_ip": datatypes.IPAddress, "sip_transports": datatypes.StringList, "trace_pjsip": datatypes.Boolean, "trace_sip": datatypes.Boolean}
+    local_ip = None
+    sip_local_udp_port = 0
+    sip_local_tcp_port = 0
+    sip_local_tls_port = 0
+    sip_transports = ["tls", "tcp", "udp"]
     trace_pjsip = False
     trace_sip = False
-
-re_host_port = re.compile("^(?P<host>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})(:(?P<port>\d+))?$")
-class SIPProxyAddress(tuple):
-    def __new__(typ, value):
-        match = re_host_port.search(value)
-        if match is None:
-            raise ValueError("invalid IP address/port: %r" % value)
-        if match.group("port") is None:
-            port = 5060
-        else:
-            port = match.group("port")
-            if port > 65535:
-                raise ValueError("port is out of range: %d" % port)
-        return match.group("host"), port
+    log_directory = '~/.sipclient/log'
 
 
 class AccountConfig(ConfigSection):
-    _datatypes = {"sip_address": str, "password": str, "display_name": str, "outbound_proxy": SIPProxyAddress}
+    _datatypes = {"sip_address": str, "password": str, "display_name": str, "outbound_proxy": OutboundProxy}
     sip_address = None
     password = None
     display_name = None
-    outbound_proxy = None, None
+    outbound_proxy = None
 
 
 process._system_config_directory = os.path.expanduser("~/.sipclient")
@@ -54,47 +46,30 @@ packet_count = 0
 start_time = None
 user_quit = True
 lock = allocate_lock()
+logger = None
 
 def event_handler(event_name, **kwargs):
-    global start_time, packet_count, queue, do_trace_pjsip
+    global start_time, packet_count, queue, do_trace_pjsip, logger
     if event_name == "siptrace":
-        if start_time is None:
-            start_time = kwargs["timestamp"]
-        packet_count += 1
-        if kwargs["received"]:
-            direction = "RECEIVED"
-        else:
-            direction = "SENDING"
-        buf = ["%s: Packet %d, +%s" % (direction, packet_count, (kwargs["timestamp"] - start_time))]
-        buf.append("%(timestamp)s: %(source_ip)s:%(source_port)d --> %(destination_ip)s:%(destination_port)d" % kwargs)
-        buf.append(kwargs["data"])
-        queue.put(("print", "\n".join(buf)))
+        logger.log(event_name, **kwargs)
     elif event_name != "log":
         queue.put(("pypjua_event", (event_name, kwargs)))
     elif do_trace_pjsip:
         queue.put(("print", "%(timestamp)s (%(level)d) %(sender)14s: %(message)s" % kwargs))
 
-def read_queue(e, username, domain, password, display_name, proxy_ip, proxy_port, target_username, target_domain, trace_sip, message, do_trace_pjsip):
+def read_queue(e, username, domain, password, display_name, route, target_uri, message):
     global user_quit, lock, queue
     lock.acquire()
     printed = False
     sent = False
     msg_buf = []
     try:
-        if proxy_ip is None:
-            # for now assume 1 SRV record and more than one A record
-            srv_answers = dns.resolver.query("_sip._udp.%s" % domain, "SRV")
-            a_answers = dns.resolver.query(str(srv_answers[0].target), "A")
-            route = Route(random.choice(a_answers).address, srv_answers[0].port)
-        else:
-            route = Route(proxy_ip, proxy_port)
         credentials = Credentials(SIPURI(user=username, host=domain, display=display_name), password)
-        if target_username is None:
+        if target_uri is None:
             reg = Registration(credentials, route=route)
-            print 'Registering "%s" at %s:%d' % (credentials.uri, route.host, route.port)
+            print 'Registering "%s" at %s:%s:%d' % (credentials.uri, route.transport, route.host, route.port)
             reg.register()
         else:
-            to_uri = SIPURI(user=target_username, host=target_domain)
             if message is None:
                 print "Press Ctrl+D on an empty line to end input and send the MESSAGE request."
             else:
@@ -137,12 +112,12 @@ def read_queue(e, username, domain, password, display_name, proxy_ip, proxy_port
                 if not sent:
                     msg_buf.append(data)
             if command == "eof":
-                if target_username is None:
+                if target_uri is None:
                     reg.unregister()
                 elif not sent:
                     sent = True
-                    print 'Sending MESSAGE from "%s" to "%s" using proxy %s:%d' % (credentials.uri, to_uri, route.host, route.port)
-                    send_message(credentials, SIPURI(user=target_username, host=target_domain), "text", "plain", "\n".join(msg_buf), route)
+                    print 'Sending MESSAGE from "%s" to "%s" using proxy %s:%s:%d' % (credentials.uri, target_uri, route.transport, route.host, route.port)
+                    send_message(credentials, target_uri, "text", "plain", "\n".join(msg_buf), route)
             if command == "quit":
                 break
     except:
@@ -150,15 +125,24 @@ def read_queue(e, username, domain, password, display_name, proxy_ip, proxy_port
         traceback.print_exc()
     finally:
         e.stop()
+        logger.stop()
         if not user_quit:
             os.kill(os.getpid(), signal.SIGINT)
         lock.release()
 
 def do_message(**kwargs):
-    global user_quit, lock, queue, do_trace_pjsip
-    do_trace_pjsip = kwargs["do_trace_pjsip"]
+    global user_quit, lock, queue, do_trace_pjsip, logger
+    do_trace_pjsip = kwargs.pop("do_trace_pjsip")
+    outbound_proxy = kwargs.pop("outbound_proxy")
     ctrl_d_pressed = False
-    e = Engine(event_handler, trace_sip=kwargs["trace_sip"], auto_sound=False, local_ip=kwargs.pop("local_ip"), local_udp_port=kwargs.pop("local_port"))
+    if outbound_proxy is None:
+        kwargs["route"] = lookup_routes_for_sip_uri(SIPURI(host=kwargs["domain"]), kwargs.pop("sip_transports"))[0]
+    else:
+        kwargs["route"] = lookup_routes_for_sip_uri(outbound_proxy, kwargs.pop("sip_transports"))[0]
+    logger = Logger(AccountConfig, GeneralConfig.log_directory, trace_sip=kwargs['trace_sip'])
+    if kwargs['trace_sip']:
+        print "Logging SIP trace to file '%s'" % logger._siptrace_filename
+    e = Engine(event_handler, trace_sip=kwargs.pop("trace_sip"), auto_sound=False, local_ip=kwargs.pop("local_ip"), local_udp_port=kwargs.pop("local_udp_port"), local_tcp_port=kwargs.pop("local_tcp_port"), local_tls_port=kwargs.pop("local_tls_port"))
     e.start()
     start_new_thread(read_queue, (e,), kwargs)
     try:
@@ -177,15 +161,11 @@ def do_message(**kwargs):
         lock.acquire()
         return
 
-def parse_host_port(option, opt_str, value, parser, host_name, port_name, default_port):
-    match = re_host_port.match(value)
-    if match is None:
-        raise OptionValueError("Could not parse supplied address: %s" % value)
-    setattr(parser.values, host_name, match.group("host"))
-    if match.group("port") is None:
-        setattr(parser.values, port_name, default_port)
-    else:
-        setattr(parser.values, port_name, int(match.group("port")))
+def parse_outbound_proxy(option, opt_str, value, parser):
+    try:
+        parser.values.outbound_proxy = OutboundProxy(value)
+    except ValueError, e:
+        raise OptionValueError(e.message)
 
 def parse_options():
     retval = {}
@@ -197,12 +177,12 @@ def parse_options():
     parser.add_option("--sip-address", type="string", dest="sip_address", help="SIP login account")
     parser.add_option("-p", "--password", type="string", dest="password", help="Password to use to authenticate the local account. This overrides the setting from the config file.")
     parser.add_option("-n", "--display-name", type="string", dest="display_name", help="Display name to use for the local account. This overrides the setting from the config file.")
-    parser.add_option("-o", "--outbound-proxy", type="string", action="callback", callback=lambda option, opt_str, value, parser: parse_host_port(option, opt_str, value, parser, "proxy_ip", "proxy_port", 5060), help="Outbound SIP proxy to use. By default a lookup is performed based on SRV and A records. This overrides the setting from the config file.", metavar="IP[:PORT]")
+    parser.add_option("-o", "--outbound-proxy", type="string", action="callback", callback=parse_outbound_proxy, help="Outbound SIP proxy to use. By default a lookup of the domain is performed based on SRV and A records. This overrides the setting from the config file.", metavar="IP[:PORT]")
     parser.add_option("-s", "--trace-sip", action="store_true", dest="trace_sip", help="Dump the raw contents of incoming and outgoing SIP messages (disabled by default).")
     parser.add_option("-m", "--message", type="string", dest="message", help="Contents of the message to send. This disables reading the message from standard input.")
     parser.add_option("-j", "--trace-pjsip", action="store_true", dest="do_trace_pjsip", help="Print PJSIP logging output (disabled by default).")
     options, args = parser.parse_args()
-    
+
     if options.account_name is None:
         account_section = "Account"
     else:
@@ -210,9 +190,12 @@ def parse_options():
     if account_section not in configuration.parser.sections():
         raise RuntimeError("There is no account section named '%s' in the configuration file" % account_section)
     configuration.read_settings(account_section, AccountConfig)
-    default_options = dict(proxy_ip=AccountConfig.outbound_proxy[0], proxy_port=AccountConfig.outbound_proxy[1], sip_address=AccountConfig.sip_address, password=AccountConfig.password, display_name=AccountConfig.display_name, trace_sip=GeneralConfig.trace_sip, message=None, do_trace_pjsip=GeneralConfig.trace_pjsip, local_ip=GeneralConfig.listen_udp[0], local_port=GeneralConfig.listen_udp[1])
+    default_options = dict(outbound_proxy=AccountConfig.outbound_proxy, sip_address=AccountConfig.sip_address, password=AccountConfig.password, display_name=AccountConfig.display_name, trace_sip=GeneralConfig.trace_sip, message=None, do_trace_pjsip=GeneralConfig.trace_pjsip, local_ip=GeneralConfig.local_ip, local_udp_port=GeneralConfig.sip_local_udp_port, local_tcp_port=GeneralConfig.sip_local_tcp_port, local_tls_port=GeneralConfig.sip_local_tls_port, sip_transports=GeneralConfig.sip_transports)
+
     options._update_loose(dict((name, value) for name, value in default_options.items() if getattr(options, name, None) is None))
-    
+
+    for transport in set(["tls", "tcp", "udp"]) - set(options.sip_transports):
+        setattr(options, "local_%s_port" % transport, None)
     if not all([options.sip_address, options.password]):
         raise RuntimeError("No complete set of SIP credentials specified in config file and on commandline.")
     for attr in default_options:
@@ -224,12 +207,9 @@ def parse_options():
     else:
         del retval["sip_address"]
     if args:
-        try:
-            retval["target_username"], retval["target_domain"] = args[0].split("@")
-        except ValueError:
-            retval["target_username"], retval["target_domain"] = args[0], retval['domain']
+        retval["target_uri"] = parse_cmdline_uri(args[0], retval["domain"])
     else:
-        retval["target_username"], retval["target_domain"] = None, None
+        retval["target_uri"] = None
     accounts = [(acc == 'Account') and 'default' or "'%s'" % acc[8:] for acc in configuration.parser.sections() if acc.startswith('Account')]
     accounts.sort()
     print "Accounts available: %s" % ', '.join(accounts)
