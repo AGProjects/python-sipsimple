516e5f8100dd2e8cddd84f4decef1bdeac878b4d
Ruud Klaver <ruud@ag-projects.com>
2009-01-15 17:08:05 +0000

    Improved behaviour of format_cmdline_uri for preprocessing SIP URIs given on the commandline

--- pypjua/clients/__init__.py
+++ pypjua/clients/__init__.py
@@ -1,18 +1,18 @@
 import re
 
-_re_pstn_num = re.compile("^\+?[0-9-() ]+$")
+_pstn_num_sub_char = "[-() ]"
+_re_pstn_num_sub = re.compile(_pstn_num_sub_char)
+_re_pstn_num = re.compile("^\+?([0-9]|%s)+$" % _pstn_num_sub_char)
 
 def format_cmdline_uri(uri, default_domain):
-    if "@" in uri:
-        if "sip:" or "sips:" in uri:
-            return uri
-        else:
-            return "sip:%s" % uri
-    else:
+    if "@" not in uri:
         if _re_pstn_num.match(uri):
-            username = uri.replace("-", "")
+            username = _re_pstn_num_sub.sub("", uri)
         else:
             username = uri
-        return "sip:%s@%s" % (username, default_domain)
+        uri = "%s@%s" % (username, default_domain)
+    if not uri.startswith("sip:") and not uri.startswith("sips:"):
+        uri = "sip:%s" % uri
+    return uri
 
 __all__ = ["format_cmdline_uri"]
