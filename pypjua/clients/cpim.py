c477aabaf91fdf998b4b0fa53512b62ef670f3c7
Denis Bilenko <denis@ag-projects.com>
2009-01-28 09:23:54 +0000

    cpim.py: fixed sips handling - used to be parsed as sip

--- pypjua/clients/cpim.py
+++ pypjua/clients/cpim.py
@@ -45,6 +45,10 @@ class SIPAddress(object):
         self.domain = domain
         self.scheme = scheme
 
+    @property
+    def secure(self):
+        return self.scheme == 'sips'
+
     @classmethod
     def parse(cls, sip_address, default_domain=None):
         if '@' in sip_address:
@@ -65,16 +69,15 @@ class SIPAddress(object):
     def __bool__(self):
         return self.username and self.domain
 
-
 _re_address = re.compile('^([^>]+)?<(.*?)>$')
-def parse_address(s):
+def parse_cpim_address(s, default_domain=None):
     """
     >>> alice = parse_address('<sip:alice@example.com>')
     >>> alice.user, alice.host, alice.display
     ('alice', 'example.com', None)
-    >>> alice = parse_address('Alice The Great <sip:alice@example.com>')
-    >>> alice.user, alice.host, alice.display
-    ('alice', 'example.com', 'Alice The Great')
+    >>> alice = parse_address('Alice The Great <sips:alice@example.com>')
+    >>> print alice
+    "Alice The Great" <sips:alice@example.com>
     """
     m = _re_address.match(s)
     if not m:
@@ -82,13 +85,13 @@ def parse_address(s):
     display, uri = m.groups()
     if display:
         display = display.strip().strip('"')
-    uri = SIPAddress.parse(uri)
-    return SIPURI(user=uri.username, host=uri.domain, display=display)
+    uri = SIPAddress.parse(uri, default_domain=default_domain)
+    return SIPURI(user=uri.username, host=uri.domain, display=display, secure=uri.secure)
 
 class MessageCPIMParser:
-    _mapping = {'From': parse_address,
-                'To': parse_address,
-                'cc': parse_address}
+    _mapping = {'From': parse_cpim_address,
+                'To': parse_cpim_address,
+                'cc': parse_cpim_address}
 
     @classmethod
     def parse_file(cls, f):
