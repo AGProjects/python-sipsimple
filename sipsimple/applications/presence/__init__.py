fb823fe910cc8171beeb68ff230f84c469326a99
Luci Stanescu <luci@ag-projects.com>
2009-02-11 15:33:48 +0000

    Added imports from presence modules in applications.presence

--- sipsimple/applications/presence/__init__.py
+++ sipsimple/applications/presence/__init__.py
@@ -1,1 +1,8 @@
-"""Presence package which contains implementations for RFC3863, RFC4479 and their extensions"""
+"""
+Presence package which contains implementations for RFC3863, RFC4479 and their extensions
+
+This module imports everything from the modules inside this package.
+"""
+
+from pypjua.applications.presence.presdm import *
+from pypjua.applications.presence.rpid import *
