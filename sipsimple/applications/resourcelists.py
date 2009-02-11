2f47fb94517fff199fe8af18dcf039ba540e308b
Luci Stanescu <luci@ag-projects.com>
2009-02-11 15:35:30 +0000

    Adapted resource lists XML application to new API

--- sipsimple/applications/resourcelists.py
+++ sipsimple/applications/resourcelists.py
@@ -12,14 +12,14 @@ Generation
 >>> rl = ResourceLists([friends])
 >>> print rl.toxml(pretty_print=True)
 <?xml version='1.0' encoding='UTF-8'?>
-<resource-lists xmlns="urn:ietf:params:xml:ns:resource-lists">
-  <list>
-    <entry uri="sip:bill@example.com">
-      <display-name>Bill Doe</display-name>
-    </entry>
-    <entry-ref ref="some/ref"/>
-  </list>
-</resource-lists>
+<rl:resource-lists xmlns:rl="urn:ietf:params:xml:ns:resource-lists">
+  <rl:list>
+    <rl:entry uri="sip:bill@example.com">
+      <rl:display-name>Bill Doe</rl:display-name>
+    </rl:entry>
+    <rl:entry-ref ref="some/ref"/>
+  </rl:list>
+</rl:resource-lists>
 <BLANKLINE>
 
 toxml() wraps etree.tostring() and accepts all its arguments (like pretty_print).
@@ -50,11 +50,11 @@ Marketing
 """
 
 import sys
-from lxml import etree
-from sipsimple.applications import ParserError, XMLMeta, XMLListApplication, XMLElement, XMLListElement, XMLStringElement
+from sipsimple.applications import ValidationError, XMLApplication, XMLListRootElement, XMLElement, XMLListElement, XMLStringElement, XMLAttribute, XMLElementChild
+from sipsimple.applications.util import SIPURI
 
 __all__ = ['_namespace_',
-           'ResourceListsMeta',
+           'ResourceListsApplication',
            'DisplayName',
            'Entry',
            'EntryRef',
@@ -88,170 +88,215 @@ _namespace_ = 'urn:ietf:params:xml:ns:resource-lists'
 # anchor must be an absolute http uri that resolves into <list>
 
 
-class ResourceListsMeta(XMLMeta): pass
+class ResourceListsApplication(XMLApplication): pass
+ResourceListsApplication.register_namespace(_namespace_, prefix='rl')
+
+
+## Marker mixins
+
+class ListElement(object): pass
+
+
+## Elements
 
 class DisplayName(XMLStringElement):
     _xml_tag = 'display-name'
     _xml_namespace = _namespace_
-    _xml_meta = ResourceListsMeta
+    _xml_application = ResourceListsApplication
     _xml_lang = True
 
-ResourceListsMeta.register(DisplayName)
 
-class Entry(XMLElement):
+class Entry(XMLElement, ListElement):
     _xml_tag = 'entry'
     _xml_namespace = _namespace_
-    _xml_attrs = {'uri': {'id_attribute': True}}
-    _xml_meta = ResourceListsMeta
+    _xml_application = ResourceListsApplication
+    _xml_children_order = {DisplayName.qname: 0}
+
+    uri = XMLAttribute('uri', type=SIPURI, required=True, test_equal=True)
+    display_name = XMLElementChild('display_name', type=DisplayName, required=False, test_equal=False)
+    _xml_id = uri
 
     def __init__(self, uri, display_name=None):
-        if uri is None:
-            raise ValueError("Entry element's uri must not be None")
+        XMLElement.__init__(self)
         self.uri = uri
-        self.display_name = display_name is not None and DisplayName(display_name) or None
-
-    def _parse_element(self, element):
-        display_name = element.find(DisplayName.qname)
-        self.display_name = display_name is not None and DisplayName.from_element(display_name) or None
-
-    def _build_element(self, element, nsmap):
-        # add display_name
-        if self.display_name:
-            self.display_name.to_element(parent=element, nsmap=nsmap)
+        self.display_name = display_name
 
     def __str__(self):
         return self.display_name and '"%s" <%s>' % (self.display_name, self.uri) or self.uri
 
-ResourceListsMeta.register(Entry)
+    def __repr__(self):
+        return '%s(%r, %r)' % (self.__class__.__name__, self.uri, self.display_name)
 
-class EntryRef(XMLElement):
+
+class EntryRef(XMLElement, ListElement):
     _xml_tag = 'entry-ref'
     _xml_namespace = _namespace_
-    _xml_attrs = {'ref': {'id_attribute': True}}
-    _xml_meta = ResourceListsMeta
+    _xml_application = ResourceListsApplication
+    _xml_children_order = {DisplayName.qname: 0}
+
+    ref = XMLAttribute('ref', type=str, required=True, test_equal=True)
+    display_name = XMLElementChild('display_name', type=DisplayName, required=False, test_equal=False)
+    _xml_id = ref
 
     def __init__(self, ref, display_name=None):
-        if ref is None:
-            raise ValueError("EntryRef element's ref must not be None")
+        XMLElement.__init__(self)
         self.ref = ref
-        self.display_name = display_name is not None and DisplayName(display_name) or None
+        self.display_name = display_name
 
-    def _parse_element(self, element):
-        display_name = element.find(DisplayName.qname)
-        self.display_name = display_name is not None and DisplayName.from_element(display_name) or None
+    def __str__(self):
+        return self.display_name and '"%s" <%s>' % (self.display_name, self.ref) or self.ref
 
-    def _build_element(self, element, nsmap):
-        # add display_name
-        if self.display_name:
-            self.display_name.to_element(parent=element, nsmap=nsmap)
+    def __repr__(self):
+        return '%s(%r, %r)' % (self.__class__.__name__, self.ref, self.display_name)
 
-ResourceListsMeta.register(EntryRef)
 
-class External(XMLElement):
+class External(XMLElement, ListElement):
     _xml_tag = 'external'
     _xml_namespace = _namespace_
-    _xml_attrs = {'anchor': {'id_attribute': True}}
-    _xml_meta = ResourceListsMeta
+    _xml_application = ResourceListsApplication
+    _xml_children_order = {DisplayName.qname: 0}
+
+    anchor = XMLAttribute('anchor', type=str, required=True, test_equal=True)
+    display_name = XMLElementChild('display_name', type=DisplayName, required=False, test_equal=False)
+    _xml_id = anchor
 
     def __init__(self, anchor, display_name=None):
-        if anchor is None:
-            raise ValueError("External element's anchor must not be None")
+        XMLElement.__init__(self)
         self.anchor = anchor
-        self.display_name = display_name is not None and DisplayName(display_name) or None
+        self.display_name = display_name
 
-    def _parse_element(self, element):
-        display_name = element.find(DisplayName.qname)
-        self.display_name = display_name is not None and DisplayName.from_element(display_name) or None
+    def __str__(self):
+        return self.display_name and '"%s" <%s>' % (self.display_name, self.anchor) or self.anchor
 
-    def _build_element(self, element, nsmap):
-        # add display_name
-        if self.display_name:
-            self.display_name.to_element(parent=element, nsmap=nsmap)
+    def __repr__(self):
+        return '%s(%r, %r)' % (self.__class__.__name__, self.anchor, self.display_name)
 
-ResourceListsMeta.register(External)
 
-class List(XMLListElement):
+class List(XMLListElement, ListElement):
     _xml_tag = 'list'
     _xml_namespace = _namespace_
-    _xml_attrs = {'name': {'id_attribute': True}}
-    _xml_meta = ResourceListsMeta
+    _xml_application = ResourceListsApplication
+    _xml_children_order = {DisplayName.qname: 0,
+                           Entry.qname: 1,
+                           EntryRef.qname: 1,
+                           External.qname: 1}
+
+    name = XMLAttribute('name', type=str, required=False, test_equal=True)
+    display_name = XMLElementChild('display_name', type=DisplayName, required=False, test_equal=False)
+    _xml_id = name
 
     def __init__(self, entries=[], name=None, display_name=None):
+        XMLListElement.__init__(self)
         self.name = name
-        self.display_name = display_name is not None and DisplayName(display_name) or None
+        self.display_name = display_name
         self[0:0] = entries
 
-    def _parse_element(self, element):
-        self.display_name = None
+    def _parse_element(self, element, *args, **kwargs):
         for child in element:
-            if child.tag == DisplayName.qname:
-                self.display_name = DisplayName.from_element(child)
-            else:
-                child_cls = self._xml_meta.get(child.tag)
-                if child_cls is not None:
-                    self.append(child_cls.from_element(child, xml_meta=self._xml_meta))
+            child_cls = self._xml_application.get_element(child.tag, None)
+            if child_cls is not None and issubclass(child_cls, ListElement):
+                try:
+                    value = child_cls.from_element(child, *args, **kwargs)
+                except ValidationError:
+                    pass
+                else:
+                    for basetype in (List, Entry, EntryRef, External):
+                        if isinstance(value, basetype):
+                            break
+                    else:
+                        list.append(self, value)
+                        continue
+                    for elem in self:
+                        if isinstance(elem, basetype) and value._xml_id == elem._xml_id:
+                            element.remove(child)
+                            break
+                    else:
+                        list.append(self, value)
+
     
-    def _build_element(self, element, nsmap):
-        # add display name
-        if self.display_name:
-            self.display_name.to_element(parent=element, nsmap=nsmap)
-        # add children
+    def _build_element(self, *args, **kwargs):
+        # build children
         for child in self:
-            child.to_element(parent=element, nsmap=nsmap)
+            child.to_element(*args, **kwargs)
     
-    def _before_add(self, value):
+    def _add_item(self, value):
         for basetype in (List, Entry, EntryRef, External):
             if isinstance(value, basetype):
                 break
         else:
-            raise TypeError("Cannot add element type %s to List" % value.__class__.__name__)
+            if isinstance(value, ListElement):
+                return value
+            raise TypeError("cannot add element type %s to List" % value.__class__.__name__)
         for elem in self:
-            if isinstance(elem, basetype) and value == elem:
-                raise ValueError("Cannot have more than one %s with the same id attribute at this level: %s" % (basetype.__name__, str(elem)))
+            if isinstance(elem, basetype) and value._xml_id == elem._xml_id:
+                raise ValueError("cannot have more than one %s with the same id attribute at this level: %r" % (basetype.__name__, value._xml_id))
+        self._insert_element(value.element)
         return value
 
+    def _del_item(self, value):
+        self.element.remove(value.element)
+
     def __str__(self):
-        return self.display_name and str(self.display_name) or str(self.name)
+        name = 'List element'
+        if self.name is not None:
+            name += ' %s' % str(self.name)
+        if self.display_name is not None:
+            name += ' (%s)' % str(self.display_name)
+        return name
 
-ResourceListsMeta.register(List)
+    def __repr__(self):
+        return '%s(%s, %r, %r)' % (self.__class__.__name__, list.__repr__(self), self.name, self.display_name)
 
-class ResourceLists(XMLListApplication):
-    accept_types = ['application/resource-lists+xml']
-    build_types = ['application/resource-lists+xml']
+List._xml_children_order[List.qname] = 1 # cannot self reference in declaration
+
+
+class ResourceLists(XMLListRootElement):
+    content_type = 'application/resource-lists+xml'
     
     _xml_tag = 'resource-lists'
     _xml_namespace = _namespace_
-    _xml_meta = ResourceListsMeta
+    _xml_application = ResourceListsApplication
+    _xml_children_order = {List.qname: 0}
     _xml_schema_file = 'resourcelists.xsd'
-    _xml_nsmap = {None: _namespace_}
-
-    _parser_opts = {'remove_blank_text': True}
 
     def __init__(self, lists=[]):
+        XMLListRootElement.__init__(self)
         self._lists = {}
         self[:] = lists
 
-    def _parse_element(self, element):
+    def _parse_element(self, element, *args, **kwargs):
         self._lists = {}
         for child in element:
-            child_cls = self._xml_meta.get(child.tag)
-            if child_cls is not None:
-                self.append(child_cls.from_element(child, xml_meta=self._xml_meta))
+            if child.tag == List.qname:
+                try:
+                    rlist = List.from_element(child, *args, **kwargs)
+                except ValidationError:
+                    pass
+                else:
+                    if rlist.name in self._lists:
+                        element.remove(child)
+                        continue
+                    list.append(self, rlist)
+                    self._lists[rlist.name] = rlist
+            else:
+                element.remove(child)
 
-    def _build_element(self, element, nsmap):
+    def _build_element(self, *args, **kwargs):
         for rlist in self:
-            rlist.to_element(parent=element, nsmap=nsmap)
+            rlist.to_element(*args, **kwargs)
 
-    def _before_add(self, rlist):
+    def _add_item(self, rlist):
         if not isinstance(rlist, List):
             raise TypeError("found %s, expected %s" % (rlist.__class__.__name__, List.__name__))
         if rlist.name in self._lists:
-            raise ValueError("Cannot have more than one list with the same name at this level: %s" % rlist.name)
+            raise ValueError("cannot have more than one list with the same name at this level: %s" % rlist.name)
         self._lists[rlist.name] = rlist
+        self._insert_element(rlist.element)
         return rlist
 
-    def _before_del(self, rlist):
+    def _del_item(self, rlist):
         del self._lists[rlist.name]
+        self.element.remove(rlist.element)
 
     # it also makes sense to be able to get a List by its name
     def __getitem__(self, key):
@@ -260,5 +305,4 @@ class ResourceLists(XMLListApplication):
         else:
             return super(ResourceLists, self).__getitem__(key)
 
-ResourceListsMeta.register(ResourceLists)
 
