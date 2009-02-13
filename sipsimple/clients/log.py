bce3de65f3f9e8c26ab1bd2ddfb3c4cd5f8e66f9
Luci Stanescu <luci@ag-projects.com>
2009-02-13 23:46:25 +0000

    Modified sipsimple.clients.log to allow logging to stdout

--- sipsimple/clients/log.py
+++ sipsimple/clients/log.py
@@ -1,12 +1,14 @@
 import os
 
+from sipsimple.clients import TraceSIPValue
+
 class Logger(object):
     def __init__(self, account, log_directory, **kwargs):
         self.account = account
         self.log_directory = log_directory
 
         # sip trace
-        self.trace_sip = kwargs.get('trace_sip', False)
+        self.trace_sip = kwargs.get('trace_sip', TraceSIPValue('none'))
         self._siptrace_filename = os.path.join(os.path.expanduser(log_directory), account.sip_address, 'sip_trace.txt')
         self._siptrace_file = None
         self._siptrace_start_time = None
@@ -25,7 +27,7 @@ class Logger(object):
 
     # log handlers
     def _LH_SCEngineSIPTrace(self, event_name, event_data):
-        if not self.trace_sip:
+        if not self.trace_sip.to_stdout and not self.trace_sip.to_file:
             return
         if self._siptrace_file is None:
             try:
@@ -44,5 +46,9 @@ class Logger(object):
         buf.append("%(timestamp)s: %(source_ip)s:%(source_port)d -(%(transport)s)-> %(destination_ip)s:%(destination_port)d" % event_data)
         buf.append(event_data["data"])
         buf.append('--\n')
-        self._siptrace_file.write("\n".join(buf))
-        self._siptrace_file.flush()
+        message = "\n".join(buf)
+        if self.trace_sip.to_file:
+            self._siptrace_file.write(message)
+            self._siptrace_file.flush()
+        if self.trace_sip.to_stdout:
+            print message
