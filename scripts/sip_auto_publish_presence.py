f7c8a4211f564d88d3424ce8efc2d6306cc831e3
Luci Stanescu <luci@ag-projects.com>
2009-02-16 12:35:58 +0000

    Added '?' key command to display control keys in sip_auto_publish_presence

--- scripts/sip_auto_publish_presence.py
+++ scripts/sip_auto_publish_presence.py
@@ -274,6 +274,13 @@ class EventHandler(object):
         elif do_trace_pjsip:
             queue.put(("print", "%(timestamp)s (%(level)d) %(sender)14s: %(message)s" % notification.data.__dict__))
 
+
+def print_control_keys():
+    print "Available control keys:"
+    print "  t: toggle SIP trace on the console"
+    print "  Ctrl-d: quit the program"
+    print "  ?: display this help message"
+
 def read_queue(e, username, domain, password, display_name, route, expires, do_trace_pjsip, interval):
     global user_quit, lock, queue, pub, sip_uri, pidf, user_agent, logger
     lock.acquire()
@@ -285,6 +292,8 @@ def read_queue(e, username, domain, password, display_name, route, expires, do_t
         pidf = PIDF(entity='%s@%s' % (username, domain))
 
         user_agent = e.user_agent
+
+        print_control_keys()
 
         #initialize auto publisher
         start_new_thread(auto_publish, (interval,))
@@ -305,6 +314,8 @@ def read_queue(e, username, domain, password, display_name, route, expires, do_t
                 if key == 't':
                     logger.trace_sip.to_stdout = not logger.trace_sip.to_stdout
                     print "SIP tracing to console is now %s" % ("activated" if logger.trace_sip.to_stdout else "deactivated")
+                elif key == '?':
+                    print_control_keys()
             if command == "eof":
                 command = "end"
                 user_quit = True
