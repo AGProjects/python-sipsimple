411aceb1b20d713b7802f49b85ae2301802a5f0f
Denis Bilenko <denis@ag-projects.com>
2009-03-18 07:46:54 +0000

    internal reorganization in GreenSession

--- sipsimple/green/session2.py
+++ sipsimple/green/session2.py
@@ -1,5 +1,6 @@
 from __future__ import with_statement
 from contextlib import contextmanager
+from application.notification import NotificationCenter
 from eventlet import coros
 from sipsimple.session import Session
 from sipsimple.green.engine import GreenBase
@@ -31,20 +32,29 @@ class GreenSession(GreenBase):
                 notification = q.wait()
                 if notification.name == 'SCSessionDidStart':
                     break
-                elif notification.name == 'SCSessionDidFail':
-                    raise SessionError(notification.data.reason)
-                elif notification.name == 'SCSessionDidEnd':
-                    if notification.data.originator == "local":
-                        raise SessionError("Session ended by local party")
-                    else:
-                        raise SessionError("Session ended by remote party")
+                else:
+                    self._raise_if_error(notification)
         # XXX I would expect Session instance not to fire SCSessionDidStart until all the transports started
         # XXX otherwise I have to go into session and manually wait for different types of events
         # XXX the same goes for terminating - wait for MSRPChatDidEnd before firing SCSessionDidEnd
+        self._wait_for_chat_to_start()
+
+    def _raise_if_error(self, notification):
+        if notification.name == 'SCSessionDidFail':
+            raise SessionError(notification.data.reason)
+        if notification.name == 'SCSessionDidEnd':
+            if notification.data.originator == "local":
+               raise SessionError("Session ended by local party")
+            else:
+                raise SessionError("Session ended by remote party")
+
+    def _wait_for_chat_to_start(self):
         if self.chat_transport is not None:
             with self.linked_notification('MSRPChatDidStart', sender=self.chat_transport) as q:
-                if not self.chat_transport.is_started:
-                    q.wait()
+                with self.linked_notifications(['SCSessionDidFail', 'SCSessionDidEnd'], queue=q):
+                    if not self.chat_transport.is_started:
+                        notification = q.wait()
+                        self._raise_if_error(notification)
 
     def accept(self, audio=False, chat=False, password=None):
         event_names = ['SCSessionDidStart',
@@ -56,29 +66,22 @@ class GreenSession(GreenBase):
                 notification = q.wait()
                 if notification.name == 'SCSessionDidStart':
                     break
-                elif notification.name == 'SCSessionDidFail':
-                    raise SessionError(notification.data.reason)
-                elif notification.name == 'SCSessionDidEnd':
-                    if notification.data.originator == "local":
-                        raise SessionError("Session ended by local party")
-                    else:
-                        raise SessionError("Session ended by remote party")
-        with self.linked_notification('MSRPChatDidStart', sender=self.chat_transport) as q:
-            if self.chat_transport is not None and not self.chat_transport.is_started:
-                q.wait()
+                else:
+                    self._raise_if_error(notification)
+        self._wait_for_chat_to_start()
 
     def terminate(self):
-         if self.state in ["NULL", "TERMINATED"]:
-             return
-         with self.linked_notifications(['SCSessionDidFail', 'SCSessionDidEnd']) as q:
-             if self.state != 'TERMINATING':
-                 self._obj.terminate()
-                 while True:
-                     notification = q.wait()
-                     if notification.name == 'SCSessionDidFail':
-                         raise SessionError(notification.data.reason)
-                     elif notification.name == 'SCSessionDidEnd':
-                         return notification
+        if self.state in ["NULL", "TERMINATED"]:
+            return
+        with self.linked_notifications(['SCSessionDidFail', 'SCSessionDidEnd']) as q:
+            if self.state != 'TERMINATING':
+                self._obj.terminate()
+                while True:
+                    notification = q.wait()
+                    if notification.name == 'SCSessionDidFail':
+                        raise SessionError(notification.data.reason)
+                    elif notification.name == 'SCSessionDidEnd':
+                        return notification
 
     def deliver_message(self, content, content_type='text/plain', to_uri=None):
         events = ['MSRPChatDidDeliverMessage', 'MSRPChatDidNotDeliverMessage']
