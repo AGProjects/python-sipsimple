2c9878b11e86193f13ad106c80140a0ccbaf3991
Luci Stanescu <luci@ag-projects.com>
2009-03-25 12:33:20 +0000

    Added makedirs function to sipsimple.util

--- sipsimple/util.py
+++ sipsimple/util.py
@@ -1,3 +1,6 @@
+import errno
+import os
+
 from zope.interface import implements
 from application.notification import IObserver
 
@@ -8,3 +11,14 @@ class NotificationHandler(object):
         handler = getattr(self, '_NH_%s' % notification.name, None)
         if handler is not None:
             handler(notification.sender, notification.data)
+
+
+def makedirs(path):
+    try:
+        os.makedirs(path)
+    except OSError, e:
+        if e.errno == errno.EEXIST and os.path.isdir(path): # directory exists
+            return
+        raise
+
+
