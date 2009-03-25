c28fbc15b9131160bad4c22fc527913cab9816d2
Denis Bilenko <denis@ag-projects.com>
2009-03-25 11:50:30 +0000

    add Engine.start_cfg() method

--- sipsimple/engine.py
+++ sipsimple/engine.py
@@ -92,6 +92,32 @@ class Engine(object):
         else:
             self._post_notification("SCEngineDidStart")
 
+    def start_cfg(self, enable_sound=True, local_ip=None, **kwargs):
+        # Take the default values for the arguments from SIPSimpleSettings
+        from sipsimple.configuration.settings import SIPSimpleSettings
+        settings = SIPSimpleSettings()
+        if local_ip is None:
+            local_ip = settings.local_ip.value
+        setdefault(kwargs,
+            local_udp_port=settings.sip.local_udp_port if "udp" in settings.sip.transports else None,
+            local_tcp_port=settings.sip.local_tcp_port if "tcp" in settings.sip.transports else None,
+            local_tls_port=settings.sip.local_tls_port if "tls" in settings.sip.transports else None,
+            tls_protocol=settings.tls.protocol,
+            tls_verify_server=settings.tls.verify_server,
+            tls_ca_file=settings.tls.ca_list_file.value if settings.tls.ca_list_file is not None else None,
+            tls_cert_file=settings.tls.certificate_file.value if settings.tls.certificate_file is not None else None,
+            tls_privkey_file=settings.tls.private_key_file.value if settings.tls.private_key_file is not None else None,
+            tls_timeout=settings.tls.timeout,
+            ec_tail_length=settings.audio.echo_delay,
+            user_agent=settings.user_agent,
+            sample_rate=settings.audio.sample_rate,
+            playback_dtmf=settings.audio.playback_dtmf,
+            rtp_port_range=(settings.rtp.port_range.start, settings.rtp.port_range.end))
+        self.start(auto_sound=False, local_ip=local_ip, **kwargs)
+        if enable_sound:
+            self.set_sound_devices(playback_device=settings.audio.output_device,
+                                   recording_device=settings.audio.input_device)
+
     def stop(self):
         if self._thread_started:
             self._thread_stopping = True
@@ -131,6 +157,10 @@ class Engine(object):
     def _post_notification(self, name, **kwargs):
         if self.notification_center is not None:
             self.notification_center.post_notification(name, self, NotificationData(timestamp=datetime.now(), **kwargs))
+
+def setdefault(where, **what):
+    for k, x in what.iteritems():
+        where.setdefault(k, x)
 
 
 class EngineStopper(object):
