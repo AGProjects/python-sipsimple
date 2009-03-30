463af14f5d5a28469615f5baa4a6c7433bc1c330
Luci Stanescu <luci@ag-projects.com>
2009-03-30 19:01:50 +0000

    Added REGISTER retry interval to output of sip_audio_session

--- scripts/sip_audio_session.py
+++ scripts/sip_audio_session.py
@@ -102,7 +102,7 @@ def read_queue(e, settings, am, account, logger, target_uri, routes, auto_answer
                 if event_name == "SIPAccountRegistrationDidSucceed":
                     print '%s Registered contact "%s" for SIP address %s (expires in %d seconds)' % (datetime.now().replace(microsecond=0), args['contact_uri'], account.id, args['registration'].expires)
                 elif event_name == "SIPAccountRegistrationDidFail":
-                    print "%s Failed to register contact for SIP address %s: %d %s" % (datetime.now().replace(microsecond=0), account.id, args['code'], args['reason'])
+                    print "%s Failed to register contact for SIP address %s: %d %s. Retrying in %.2f seconds." % (datetime.now().replace(microsecond=0), account.id, args['code'], args['reason'], args['delay'])
                     command = "quit"
                     user_quit = False
                 elif event_name == "SIPAccountRegistrationDidEnd":
