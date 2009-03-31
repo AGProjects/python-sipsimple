97e8f52cb0dd14c34efd9ccb2c169d95158394aa
Luci Stanescu <luci@ag-projects.com>
2009-03-31 11:47:53 +0000

    Added tests to verify account can be used to publish presence

--- scripts/sip_publish_presence.py
+++ scripts/sip_publish_presence.py
@@ -21,7 +21,7 @@ from twisted.internet import reactor
 from eventlet.twistedutil import join_reactor
 
 from sipsimple import Engine, Publication, SIPCoreError, SIPURI
-from sipsimple.account import AccountManager
+from sipsimple.account import AccountManager, BonjourAccount
 from sipsimple.clients.log import Logger
 from sipsimple.lookup import DNSLookup
 from sipsimple.configuration import ConfigurationManager
@@ -473,6 +473,10 @@ class PublicationApplication(object):
             raise RuntimeError("unknown account %s. Available accounts: %s" % (self.account_name, ', '.join(account.id for account in account_manager.iter_accounts())))
         elif not self.account.enabled:
             raise RuntimeError("account %s is not enabled" % self.account.id)
+        elif self.account == BonjourAccount():
+            raise RuntimeError("cannot use bonjour account to publish presence information")
+        elif not self.account.presence.enabled:
+            raise RuntimeError("presence is not enabled for account %s" % self.account.id)
         self.output.put('Using account %s' % self.account.id)
         settings = SIPSimpleSettings()
 
