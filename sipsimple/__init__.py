b219859ac813983ef6ca97441d877779dca2a1a2
Ruud Klaver <ruud@ag-projects.com>
2009-04-09 13:24:38 +0000

    Removed imports from sipsimple.__init__

--- sipsimple/__init__.py
+++ sipsimple/__init__.py
@@ -8,20 +8,4 @@ from core import CORE_REVISION
 if CORE_REVISION != _revision_required:
     raise ImportError("Wrong SIP core revision %d (expected %d)" % (CORE_REVISION, _revision_required))
 
-from engine import Engine
-from core import SIPURI, Credentials, Route
-from core import Request, Registration, Publication, Subscription, Invitation, send_message
-from core import SDPAttribute, SDPConnection, SDPMedia, SDPSession
-from core import RTPTransport, AudioTransport
-from core import SIPCoreError, PJSIPError
-from core import WaveFile, RecordingWaveFile
-from session import Session, SessionManager
-
-__all__ = ["Engine",
-           "SIPURI", "Credentials", "Route",
-           "Request", "Registration", "Publication", "Subscription", "Invitation", "send_message",
-           "SDPAttribute", "SDPConnection", "SDPMedia", "SDPSession",
-           "RTPTransport", "AudioTransport",
-           "SIPCoreError", "PJSIPError",
-           "WaveFile", "RecordingWaveFile",
-           "Session", "SessionManager"]
+__all__ = ["__version__"]
