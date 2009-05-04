c659310082308e19429e14872ff34431d2549f48
Ruud Klaver <ruud@ag-projects.com>
2009-05-04 10:57:15 +0000

    Added Benny's PJSIP patch for cancelling a remote SDP offer
    To make sure this patch is applied, perform the following command:
    > python setup.py build_ext --pjsip-clean-compile

--- setup_pjsip.py
+++ setup_pjsip.py
@@ -55,7 +55,8 @@ class PJSIP_build_ext(build_ext):
                    "#define PJSIP_MAX_PKT_LEN 65536",
                    "#define PJSIP_UNESCAPE_IN_PLACE 1",
                    "#define PJMEDIA_HAS_L16_CODEC 0"]
-    patch_files = ["patches/pjsip-2371-sip_inv-on_rx_reinvite.patch",
+    patch_files = ["patches/sdp_neg_cancel_remote_offer_r2669.patch",
+                   "patches/pjsip-2371-sip_inv-on_rx_reinvite.patch",
                    "patches/pjsip-2425-sdp_media_line.patch",
                    "patches/pjsip-2553-sip_inv-recv_neg_reply_to_reinvite.patch",
                    "patches/pjsip-2553-sip_inv-dont_disconnect_on_408_reply_to_reinvite.patch"]
