12b5c3e8c5e9361e0df3be0c4c4d0efdc05e102b
Ruud Klaver <ruud@ag-projects.com>
2009-05-08 14:40:04 +0000

    Fixed bug where local_ip argument was not used in Engine.start()

--- sipsimple/engine.py
+++ sipsimple/engine.py
@@ -68,7 +68,7 @@ class Engine(Thread):
         if self._thread_started:
             raise SIPCoreError("Worker thread was already started once")
         init_options = Engine.default_start_options.copy()
-        init_options.update(kwargs)
+        init_options.update(kwargs, local_ip=local_ip)
         self._post_notification("SIPEngineWillStart")
         with self._lock:
             try:
