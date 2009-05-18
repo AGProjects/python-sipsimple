d0bbd861619239cbf73dbc880a49aaaf4ead776a
Ruud Klaver <ruud@ag-projects.com>
2009-05-18 14:12:48 +0000

    Adapted sip_subscribe_xcap_diff.py to the new Credentials class

--- scripts/sip_subscribe_xcap_diff.py
+++ scripts/sip_subscribe_xcap_diff.py
@@ -235,7 +235,7 @@ class SubscriptionApplication(object):
                     timeout = random.uniform(self._subscription_wait, 2*self._subscription_wait)
                     reactor.callFromThread(reactor.callLater, timeout, self._subscribe)
                 else:
-                    self.subscription = Subscription(self.account.credentials, self.target, "xcap-diff", route=self._subscription_routes.popleft(), expires=self.account.presence.subscribe_interval)
+                    self.subscription = Subscription(self.account.uri, self.target, "xcap-diff", route=self._subscription_routes.popleft(), credentials=self.account.credentials, expires=self.account.presence.subscribe_interval)
                     notification_center = NotificationCenter()
                     notification_center.add_observer(self, sender=self.subscription)
                     self.subscription.subscribe()
@@ -248,7 +248,7 @@ class SubscriptionApplication(object):
     def _NH_DNSLookupDidSucceed(self, notification):
         # create subscription and register to get notifications from it
         self._subscription_routes = deque(notification.data.result)
-        self.subscription = Subscription(self.account.credentials, self.target, "xcap-diff", route=self._subscription_routes.popleft(), expires=self.account.presence.subscribe_interval)
+        self.subscription = Subscription(self.account.uri, self.target, "xcap-diff", route=self._subscription_routes.popleft(), credentials=self.account.credentials, expires=self.account.presence.subscribe_interval)
         notification_center = NotificationCenter()
         notification_center.add_observer(self, sender=self.subscription)
         self.subscription.subscribe()
