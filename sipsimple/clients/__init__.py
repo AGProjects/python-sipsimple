b6b3f607ba473385ef3fc3092611899d1f234e4d
Ruud Klaver <ruud@ag-projects.com>
2009-05-18 07:32:41 +0000

    Append domain to SIP URI if script user uses a domain without a "."

--- sipsimple/clients/__init__.py
+++ sipsimple/clients/__init__.py
@@ -16,6 +16,8 @@ def format_cmdline_uri(uri, default_domain):
         else:
             username = uri
         uri = "%s@%s" % (username, default_domain)
+    elif "." not in uri.split("@", 1)[1]:
+        uri += "." + default_domain
     if not uri.startswith("sip:") and not uri.startswith("sips:"):
         uri = "sip:%s" % uri
     return uri
