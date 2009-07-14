6db366b30565c168d73c018a84ac09126096a3a7
Luci Stanescu <luci@ag-projects.com>
2009-07-14 16:06:52 +0000

    Indended sipsimple/interfaces.py correctly

--- sipsimple/interfaces.py
+++ sipsimple/interfaces.py
@@ -1,31 +1,32 @@
 from zope.interface import Interface, Attribute
 
 class IMediaStream(Interface):
-     on_hold_by_local = Attribute("True if the stream is on hold by the local party")
-     on_hold_by_remote = Attribute("True if the stream is on hold by the remeot")
-     on_hold = Attribute("True if either on_hold_by_local or on_hold_by_remote is true") 
+    on_hold_by_local = Attribute("True if the stream is on hold by the local party")
+    on_hold_by_remote = Attribute("True if the stream is on hold by the remeot")
+    on_hold = Attribute("True if either on_hold_by_local or on_hold_by_remote is true") 
 
-     def __init__(self, account):
-         pass
+    def __init__(self, account):
+        pass
 
-     def get_local_media(self, for_offer, on_hold):
-         pass
+    def get_local_media(self, for_offer, on_hold):
+        pass
 
-     def validate_incoming(self, remote_sdp, stream_index):
-         pass
+    def validate_incoming(self, remote_sdp, stream_index):
+        pass
 
-     def initialize(self, session):
-         pass
+    def initialize(self, session):
+        pass
 
-     def start(self, local_sdp, remote_sdp, stream_index):
-         pass
+    def start(self, local_sdp, remote_sdp, stream_index):
+        pass
 
-     def end(self):
-         pass
+    def end(self):
+        pass
 
-     def validate_update(self, remote_sdp, stream_index):
-         pass
+    def validate_update(self, remote_sdp, stream_index):
+        pass
 
-     def update(self, local_sdp, remote_sdp, stream_index):
-         pass
+    def update(self, local_sdp, remote_sdp, stream_index):
+        pass
+
 
