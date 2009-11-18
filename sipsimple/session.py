1d61f16e64bc22a4c807f9d2a19c12d2ff27d734
Adrian Georgescu <ag@ag-projects.com>
2009-11-18 17:31:56 +0000

    Update project description

--- sipsimple/session.py
+++ sipsimple/session.py
@@ -3,7 +3,7 @@
 
 """Implements an asynchronous notification based mechanism for
 establishment, modification and termination of sessions using Session
-Initation Protocol (SIP) standardized in RFC3261
+Initiation Protocol (SIP) standardized in RFC3261
 """
 
 from __future__ import with_statement
