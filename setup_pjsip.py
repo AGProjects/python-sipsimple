bdcafaf6b238d3981cadf7557a126697a66a5785
Saul Ibarra <saul@ag-projects.com>
2009-12-08 13:24:24 +0000

    Don't bit-shift g722 audio samples

--- setup_pjsip.py
+++ setup_pjsip.py
@@ -77,7 +77,8 @@ class PJSIP_build_ext(build_ext):
                    "patches/pjsip-2394-sip_dialog-no_totag_check_on_dialog_state_update.patch",
                    "patches/pjsip-2832-sdp_ignore_missing_rtpmap_for_dynamic_pt.patch",
                    "patches/pjsip-2833-parse_pjsip_allow_events_hdr.patch",
-                   "patches/pjsip-2830-runtime_device_change_detection.patch"]
+                   "patches/pjsip-2830-runtime_device_change_detection.patch",
+                   "patches/pjsip-2342-g722-14-bits-conversion.patch"]
     pjsip_svn_repos = {"trunk": "http://svn.pjsip.org/repos/pjproject/trunk",
                        "1.0": "http://svn.pjsip.org/repos/pjproject/branches/1.0"}
     portaudio_patch_files = ["patches/portaudio-1420-runtime_device_change_detection.patch",
