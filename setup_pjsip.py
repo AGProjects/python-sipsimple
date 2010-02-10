cf8a7f0299b8ced6a66e6fa4dd05df14fe91a82e
Saul Ibarra <saul@ag-projects.com>
2010-02-10 16:36:29 +0000

    Use regular nomination in ICE instead of aggressive

--- setup_pjsip.py
+++ setup_pjsip.py
@@ -81,7 +81,9 @@ class PJSIP_build_ext(build_ext):
                    "patches/pjsip-2342-g722-14-bits-conversion.patch",
                    "patches/pjsip-2656-ip_selection_algorithm.patch",
                    "patches/pjsip-2830-allow_cancel_reinvite.patch",
-                   "patches/pjsip-2830-ice_priority_calculation.patch"]
+                   "patches/pjsip-2830-ice_priority_calculation.patch",
+                   "patches/pjsip-2830-ice_regular_nomination.patch",
+                   "patches/pjsip-2830-dont_compile_pjsua.patch"]
     pjsip_svn_repos = {"trunk": "http://svn.pjsip.org/repos/pjproject/trunk",
                        "1.0": "http://svn.pjsip.org/repos/pjproject/branches/1.0"}
     portaudio_patch_files = ["patches/portaudio-1420-runtime_device_change_detection.patch",
