8dd9b7368a4b9e3145add6fdadd9b430941cb0b2
Dan Pascu <dan@ag-projects.com>
2010-11-03 14:04:46 +0000

    Use None when the server advertised voicemail URI is an empty string

--- sipsimple/account.py
+++ sipsimple/account.py
@@ -427,7 +427,7 @@ class AccountMWISubscriptionHandler(object):
                                             except ValidationError:
                                                 pass
                                             else:
-                                                self.account.message_summary.server_advertised_uri = message_summary.message_account.replace('sip:', '', 1)
+                                                self.account.message_summary.server_advertised_uri = message_summary.message_account.replace('sip:', '', 1) or None
                                                 notification_center.post_notification('SIPAccountMWIDidGetSummary', sender=self.account, data=TimestampedNotificationData(message_summary=message_summary))
                                         break
                         except api.TimeoutError:
@@ -491,7 +491,7 @@ class AccountMWISubscriptionHandler(object):
             except ValidationError:
                 pass
             else:
-                self.account.message_summary.server_advertised_uri = message_summary.message_account.replace('sip:', '', 1)
+                self.account.message_summary.server_advertised_uri = message_summary.message_account.replace('sip:', '', 1) or None
                 NotificationCenter().post_notification('SIPAccountMWIDidGetSummary', sender=self.account, data=TimestampedNotificationData(message_summary=message_summary))
 
     def _NH_SystemIPAddressDidChange(self, notification):
