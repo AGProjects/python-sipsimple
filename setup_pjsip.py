7798485ac1621ce0e672c08536f186ca8b683a4c
Saul Ibarra <saul@ag-projects.com>
2011-03-25 09:00:00 +0000

    Fixed SDP negotiation on bogus answer
    Ticket in PJSIP: https://trac.pjsip.org/repos/ticket/1066
    Applied changesets: 3160, 3187

--- setup_pjsip.py
+++ setup_pjsip.py
@@ -114,7 +114,8 @@ class PJSIP_build_ext(build_ext):
                    "patches/pjsip-2830-fix_crash_with_retry_after_header.patch",
                    "patches/pjsip-3368-disable_assert_on_invite_answer.patch",
                    "patches/pjsip-3368-evsub_timer_functions.patch",
-                   "patches/pjsip-3368-remove_hdr_by_name.patch"]
+                   "patches/pjsip-3368-remove_hdr_by_name.patch",
+                   "patches/pjsip-3187-sdp_neg_fix_on_bogus_answer.patch"]
     pjsip_svn_repos = {"trunk": "http://svn.pjsip.org/repos/pjproject/trunk",
                        "1.0": "http://svn.pjsip.org/repos/pjproject/branches/1.0"}
     portaudio_patch_files = ["patches/portaudio-1420-runtime_device_change_detection.patch",
