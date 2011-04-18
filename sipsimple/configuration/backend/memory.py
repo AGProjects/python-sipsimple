bce640b2bcf0efafe56f00851b7930d5c3016d3a
Dan Pascu <dan@ag-projects.com>
2011-04-18 08:25:56 +0000

    Added configuration memory backend

---
+++ sipsimple/configuration/backend/memory.py
@@ -0,0 +1,26 @@
+# Copyright (C) 2011 AG Projects. See LICENSE for details.
+#
+
+"""Configuration backend for storing settings in memory"""
+
+__all__ = ["MemoryBackend"]
+
+from zope.interface import implements
+from sipsimple.configuration.backend import IConfigurationBackend
+
+
+class MemoryBackend(object):
+    """Implementation of a configuration backend that stores data in memory."""
+
+    implements(IConfigurationBackend)
+
+    def __init__(self):
+        self.data = {}
+
+    def load(self):
+        return self.data
+
+    def save(self, data):
+        self.data = data
+
+
