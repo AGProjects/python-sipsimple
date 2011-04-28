f090edc9cae53835301ea210c57cd0d73ca20cf6
Saul Ibarra <saul@ag-projects.com>
2011-04-28 10:01:55 +0000

    Raised core revision

--- sipsimple/core/__init__.py
+++ sipsimple/core/__init__.py
@@ -5,7 +5,7 @@ from sipsimple.core._core import *
 from sipsimple.core._engine import *
 from sipsimple.core._primitives import *
 
-required_revision = 137
+required_revision = 138
 if CORE_REVISION != required_revision:
     raise ImportError("Wrong SIP core revision %d (expected %d)" % (CORE_REVISION, required_revision))
 del required_revision
