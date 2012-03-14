4de4936e881461c90c51d3eac202a9060d095f9c
Saul Ibarra <saul@ag-projects.com>
2012-03-14 10:09:41 +0000

    ROLLBACK: Removed no longer needed function
    The builtin function will raise ValueError if the iterable doesn't
    contain at least 'r' elementsi, this one doesn't
    
    rolling back:
    
    Tue Feb  7 16:00:09 CET 2012  Saul Ibarra <saul@ag-projects.com>
      * Removed no longer needed function
    
        M ./sipsimple/audio.py -1 +2
        M ./sipsimple/util.py -25 +1

--- sipsimple/audio.py
+++ sipsimple/audio.py
@@ -10,7 +10,6 @@ __all__ = ['IAudioPort', 'AudioDevice', 'AudioBridge', 'RootAudioBridge', 'WaveP
 import os
 import weakref
 from functools import partial
-from itertools import combinations
 from threading import RLock
 
 from application.notification import IObserver, NotificationCenter, ObserverWeakrefProxy
@@ -21,7 +20,7 @@ from zope.interface import Attribute, Interface, implements
 from sipsimple.core import MixerPort, RecordingWaveFile, SIPCoreError, WaveFile
 from sipsimple.threading import run_in_twisted_thread
 from sipsimple.threading.green import Command, run_in_waitable_green_thread
-from sipsimple.util import TimestampedNotificationData
+from sipsimple.util import TimestampedNotificationData, combinations
 
 
 class WavePlayerError(Exception): pass
--- sipsimple/util.py
+++ sipsimple/util.py
@@ -5,7 +5,7 @@
 
 from __future__ import absolute_import, with_statement
 
-__all__ = ["All", "Any", "MultilingualText", "Route", "Timestamp", "TimestampedNotificationData", "user_info"]
+__all__ = ["All", "Any", "MultilingualText", "Route", "Timestamp", "TimestampedNotificationData", "combinations", "user_info"]
 
 import os
 import platform
@@ -191,6 +191,30 @@ class TimestampedNotificationData(NotificationData):
     def __init__(self, **kwargs):
         self.timestamp = datetime.now()
         NotificationData.__init__(self, **kwargs)
+
+
+# Utility functions
+#
+
+def combinations(iterable, r):
+    # combinations('ABCD', 2) --> AB AC AD BC BD CD
+    # combinations(range(4), 3) --> 012 013 023 123
+    pool = tuple(iterable)
+    n = len(pool)
+    if r > n:
+        return
+    indices = range(r)
+    yield tuple(pool[i] for i in indices)
+    while True:
+        for i in reversed(range(r)):
+            if indices[i] != i + n - r:
+                break
+        else:
+            return
+        indices[i] += 1
+        for j in range(i+1, r):
+            indices[j] = indices[j-1] + 1
+        yield tuple(pool[i] for i in indices)
 
 
 # Utility objects
