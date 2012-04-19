00fa6cb0c5c7b2ff73370c6ed6f095a03ebd27a2
Dan Pascu <dan@ag-projects.com>
2012-04-19 13:42:18 +0000

    Refactored dialog rules to reuse elements from oma common policy

--- sipsimple/payloads/dialogrules.py
+++ sipsimple/payloads/dialogrules.py
@@ -9,21 +9,26 @@ NOTE: Subscription Handling has been taken from RFC 5025.
 """
 
 
-__all__ = ['cp_namespace', 'dlg_namespace', 'DialogRulesDocument', 'ExternalList', 'SubHandling', 'DialogRules']
+__all__ = ['cp_namespace', 'dlg_namespace', 'DialogRulesDocument', 'SubHandling', 'DialogRules']
 
 
-from sipsimple.payloads import XMLElementID, XMLElement, XMLListElement, XMLStringElement
-from sipsimple.payloads.datatypes import AnyURI
-from sipsimple.payloads.policy import namespace as cp_namespace, CommonPolicyDocument, ActionElement, ConditionElement, RuleSet
+from sipsimple.payloads import omapolicy
+from sipsimple.payloads import XMLStringElement
+from sipsimple.payloads.policy import namespace as cp_namespace, CommonPolicyDocument, ActionElement, RuleSet
 
 
 dlg_namespace = 'http://openxcap.org/ns/dialog-rules'
 
+
 class DialogRulesDocument(CommonPolicyDocument): pass
-DialogRulesDocument.register_namespace(dlg_namespace, prefix='dr')
+DialogRulesDocument.register_namespace(dlg_namespace, prefix='dr', schema='dialog-rules.xsd')
+DialogRulesDocument.register_namespace(omapolicy.oma_cp_namespace, prefix='ocp', schema='oma-common-policy.xsd')
+DialogRulesDocument.register_element(omapolicy.AnonymousRequest)
+DialogRulesDocument.register_element(omapolicy.OtherIdentity)
+DialogRulesDocument.register_element(omapolicy.Entry)
+DialogRulesDocument.register_element(omapolicy.ExternalList)
 
 
-## Attribute value types
 class SubHandlingValue(str):
     __prioritymap__ = {'block': 0, 'confirm': 10, 'polite-block': 20, 'allow': 30}
 
@@ -37,61 +42,11 @@ class SubHandlingValue(str):
         return self.__prioritymap__[self]
 
 
-## Action Elements
 class SubHandling(XMLStringElement, ActionElement):
     _xml_tag = 'sub-handling'
     _xml_namespace = dlg_namespace
     _xml_document = DialogRulesDocument
     _xml_value_type = SubHandlingValue
-
-
-## Condition Elements
-class Entry(XMLElement):
-    _xml_tag = 'entry'
-    _xml_namespace = dlg_namespace
-    _xml_document = DialogRulesDocument
-
-    uri = XMLElementID('uri', xmlname='anc', type=AnyURI, required=True, test_equal=True)
-
-    def __init__(self, uri):
-        XMLElement.__init__(self)
-        self.uri = uri
-
-    def __unicode__(self):
-        return self.uri
-
-    def __str__(self):
-        return str(self.uri)
-
-
-class ExternalList(XMLListElement, ConditionElement):
-    _xml_tag = 'external-list'
-    _xml_namespace = dlg_namespace
-    _xml_document = DialogRulesDocument
-    _xml_item_type = Entry
-
-    def __init__(self, entries=[]):
-        XMLListElement.__init__(self)
-        self.update(entries)
-
-    def __iter__(self):
-        return (unicode(item) for item in super(ExternalList, self).__iter__())
-
-    def __repr__(self):
-        return '%s(%r)' % (self.__class__.__name__, list(self))
-
-    def add(self, item):
-        if isinstance(item, basestring):
-            item = Entry(item)
-        super(ExternalList, self).add(item)
-
-    def remove(self, item):
-        if isinstance(item, basestring):
-            try:
-                item = (entry for entry in super(ExternalList, self).__iter__() if entry == item).next()
-            except StopIteration:
-                raise KeyError(item)
-        super(ExternalList, self).remove(item)
 
 
 class DialogRules(RuleSet):
