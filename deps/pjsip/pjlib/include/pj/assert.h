d960e038e32ea7eaa479948dfea36264f187c976
Saul Ibarra <saul@ag-projects.com>
2013-06-06 14:44:10 +0000

    pjsip: Disable assertions on recoverable errors

--- deps/pjsip/pjlib/include/pj/assert.h
+++ deps/pjsip/pjlib/include/pj/assert.h
@@ -63,7 +63,7 @@
 #if defined(PJ_ENABLE_EXTRA_CHECK) && PJ_ENABLE_EXTRA_CHECK != 0
 #   define PJ_ASSERT_RETURN(expr,retval)    \
 	    do { \
-		if (!(expr)) { pj_assert(expr); return retval; } \
+		if (!(expr)) { return retval; } \
 	    } while (0)
 #else
 #   define PJ_ASSERT_RETURN(expr,retval)    pj_assert(expr)
@@ -82,7 +82,6 @@
 #if defined(PJ_ENABLE_EXTRA_CHECK) && PJ_ENABLE_EXTRA_CHECK != 0
 #   define PJ_ASSERT_ON_FAIL(expr,exec_on_fail)    \
 	    do { \
-		pj_assert(expr); \
 		if (!(expr)) exec_on_fail; \
 	    } while (0)
 #else
