0d59ec55b9ed1521366d022094887458b219d509
Saul Ibarra <saul@ag-projects.com>
2013-07-22 15:44:58 +0000

    pjsip: Add AGC and high pass filter to audio processing

--- deps/pjsip/pjmedia/src/pjmedia/echo_webrtc_aec.c
+++ deps/pjsip/pjmedia/src/pjmedia/echo_webrtc_aec.c
@@ -40,6 +40,7 @@
 
 #include <third_party/webrtc/src/common_audio/signal_processing_library/main/interface/signal_processing_library.h>
 #include <third_party/webrtc/src/modules/audio_processing/aec/main/interface/echo_cancellation.h>
+#include <third_party/webrtc/src/modules/audio_processing/agc/main/interface/gain_control.h>
 #include <third_party/webrtc/src/modules/audio_processing/ns/main/interface/noise_suppression.h>
 
 #include "echo_internal.h"
@@ -148,17 +149,104 @@ static int AudioBuffer_SamplesPerChannel(AudioBuffer *ab)
 }
 
 
+const WebRtc_Word16 kFilterCoefficients8kHz[5] =
+    {3798, -7596, 3798, 7807, -3733};
+
+const WebRtc_Word16 kFilterCoefficients[5] =
+    {4012, -8024, 4012, 8002, -3913};
+
+typedef struct {
+  WebRtc_Word16 y[4];
+  WebRtc_Word16 x[2];
+  const WebRtc_Word16* ba;
+} HighPassFilterState;
+
+
+static int HighPassFilter_Initialize(HighPassFilterState* hpf, int sample_rate) {
+  assert(hpf != NULL);
+
+  if (sample_rate == 8000) {
+    hpf->ba = kFilterCoefficients8kHz;
+  } else {
+    hpf->ba = kFilterCoefficients;
+  }
+
+  WebRtcSpl_MemSetW16(hpf->x, 0, 2);
+  WebRtcSpl_MemSetW16(hpf->y, 0, 4);
+
+  return 0;
+}
+
+
+static int HighPassFilter_Process(HighPassFilterState* hpf, WebRtc_Word16* data, int length) {
+  assert(hpf != NULL);
+
+  int i;
+  WebRtc_Word32 tmp_int32 = 0;
+  WebRtc_Word16* y = hpf->y;
+  WebRtc_Word16* x = hpf->x;
+  const WebRtc_Word16* ba = hpf->ba;
+
+  for (i = 0; i < length; i++) {
+    //  y[i] = b[0] * x[i] + b[1] * x[i-1] + b[2] * x[i-2]
+    //         + -a[1] * y[i-1] + -a[2] * y[i-2];
+
+    tmp_int32 = WEBRTC_SPL_MUL_16_16(y[1], ba[3]); // -a[1] * y[i-1] (low part)
+    tmp_int32 += WEBRTC_SPL_MUL_16_16(y[3], ba[4]); // -a[2] * y[i-2] (low part)
+    tmp_int32 = (tmp_int32 >> 15);
+    tmp_int32 += WEBRTC_SPL_MUL_16_16(y[0], ba[3]); // -a[1] * y[i-1] (high part)
+    tmp_int32 += WEBRTC_SPL_MUL_16_16(y[2], ba[4]); // -a[2] * y[i-2] (high part)
+    tmp_int32 = (tmp_int32 << 1);
+
+    tmp_int32 += WEBRTC_SPL_MUL_16_16(data[i], ba[0]); // b[0]*x[0]
+    tmp_int32 += WEBRTC_SPL_MUL_16_16(x[0], ba[1]);    // b[1]*x[i-1]
+    tmp_int32 += WEBRTC_SPL_MUL_16_16(x[1], ba[2]);    // b[2]*x[i-2]
+
+    // Update state (input part)
+    x[1] = x[0];
+    x[0] = data[i];
+
+    // Update state (filtered part)
+    y[2] = y[0];
+    y[3] = y[1];
+    y[0] = (WebRtc_Word16)(tmp_int32 >> 13);
+    y[1] = (WebRtc_Word16)((tmp_int32 - WEBRTC_SPL_LSHIFT_W32((WebRtc_Word32)(y[0]), 13)) << 2);
+
+    // Rounding in Q12, i.e. add 2^11
+    tmp_int32 += 2048;
+
+    // Saturate (to 2^27) so that the HP filtered signal does not overflow
+    tmp_int32 = WEBRTC_SPL_SAT((WebRtc_Word32)(134217727), tmp_int32, (WebRtc_Word32)(-134217728));
+
+    // Convert back to Q0 and use rounding
+    data[i] = (WebRtc_Word16)WEBRTC_SPL_RSHIFT_W32(tmp_int32, 12);
+
+  }
+
+  return 0;
+}
+
+
 typedef struct webrtc_ec
 {
     void        *AEC_inst;
+    void        *AGC_inst;
     NsHandle    *NS_inst;
     pj_bool_t   needs_reset;
+
     unsigned    clock_rate;
     unsigned	echo_tail;
     unsigned    samples_per_frame;
     unsigned    samples_per_10ms_frame;
+
+    WebRtc_Word32   mic_capture_level;
+    WebRtc_Word16   has_echo;
+    WebRtc_UWord8   is_saturated;
+
+    HighPassFilterState  hpf;
     AudioBuffer capture_audio_buffer;
     AudioBuffer playback_audio_buffer;
+
     pj_int16_t	*tmp_frame;
 } webrtc_ec;
 
@@ -167,6 +255,12 @@ typedef struct webrtc_ec
     do {                                                                  \
         unsigned status = WebRtcAec_get_error_code(aec_inst);             \
         PJ_LOG(4, (THIS_FILE, "WebRTC AEC ERROR (%s) %d", tag, status));  \
+    } while (0)                                                           \
+
+
+#define WEBRTC_AGC_ERROR(ns_inst, text)                                   \
+    do {                                                                  \
+        PJ_LOG(4, (THIS_FILE, "WebRTC AGC ERROR (%s)", text));            \
     } while (0)                                                           \
 
 
@@ -220,6 +314,29 @@ PJ_DEF(pj_status_t) webrtc_aec_create(pj_pool_t *pool,
     	goto error;
     }
 
+    status = WebRtcAgc_Create(&echo->AGC_inst);
+    if(status != 0) {
+        PJ_LOG(4, (THIS_FILE, "Couldn't allocate memory for WebRTC AGC"));
+    	goto error;
+    }
+
+    status = WebRtcAgc_Init(echo->AGC_inst, 0, 255, kAgcModeAdaptiveAnalog, clock_rate);
+    if(status != 0) {
+        WEBRTC_AGC_ERROR(echo->AGC_inst, "initialization");
+    	goto error;
+    }
+
+    WebRtcAgc_config_t agc_config;
+    agc_config.targetLevelDbfs = 6;
+    agc_config.compressionGaindB = 9;
+    agc_config.limiterEnable = kAgcTrue;
+
+    status = WebRtcAgc_set_config(echo->AGC_inst, agc_config);
+    if(status != 0) {
+        WEBRTC_AGC_ERROR(echo->AGC_inst, "config initialization");
+    	goto error;
+    }
+
     status = WebRtcNs_Create(&echo->NS_inst);
     if(status != 0) {
         PJ_LOG(4, (THIS_FILE, "Couldn't allocate memory for WebRTC NS"));
@@ -242,6 +359,7 @@ PJ_DEF(pj_status_t) webrtc_aec_create(pj_pool_t *pool,
     echo->samples_per_10ms_frame = clock_rate / 100;    /* the WebRTC engine works with 10ms frames */
     echo->echo_tail = tail_ms;
     echo->needs_reset = PJ_FALSE;
+    echo->mic_capture_level = 255;    /* initial mic capture level, maximum */
 
     /* Allocate temporary frames for echo cancellation */
     echo->tmp_frame = (pj_int16_t*) pj_pool_zalloc(pool, 2*samples_per_frame);
@@ -251,6 +369,9 @@ PJ_DEF(pj_status_t) webrtc_aec_create(pj_pool_t *pool,
     AudioBuffer_Initialize(&echo->capture_audio_buffer, clock_rate);
     AudioBuffer_Initialize(&echo->playback_audio_buffer, clock_rate);
 
+    /* Initialize high pass filter */
+    HighPassFilter_Initialize(&echo->hpf, clock_rate);
+
     PJ_LOG(4, (THIS_FILE, "WebRTC AEC and NS initialized"));
     *p_echo = echo;
     return PJ_SUCCESS;
@@ -258,6 +379,8 @@ PJ_DEF(pj_status_t) webrtc_aec_create(pj_pool_t *pool,
 error:
     if (echo->AEC_inst)
         WebRtcAec_Free(echo->AEC_inst);
+    if (echo->AGC_inst)
+        WebRtcAgc_Free(echo->AGC_inst);
     if (echo->NS_inst)
         WebRtcNs_Free(echo->NS_inst);
     return PJ_EBUG;
@@ -272,6 +395,10 @@ PJ_DEF(pj_status_t) webrtc_aec_destroy(void *state )
     if (echo->AEC_inst) {
     	WebRtcAec_Free(echo->AEC_inst);
     	echo->AEC_inst = NULL;
+    }
+    if (echo->AGC_inst) {
+    	WebRtcAgc_Free(echo->AGC_inst);
+    	echo->AGC_inst = NULL;
     }
     if (echo->NS_inst) {
         WebRtcNs_Free(echo->NS_inst);
@@ -291,24 +418,11 @@ PJ_DEF(void) webrtc_aec_reset(void *state)
 
 static void aec_reset(webrtc_ec *echo)
 {
-    PJ_ASSERT_ON_FAIL(echo && echo->AEC_inst && echo->NS_inst, {return;});
+    PJ_ASSERT_ON_FAIL(echo && echo->AEC_inst && echo->AGC_inst && echo->NS_inst, {return;});
 
     int status = 0;
 
-    /* re-initialize the NS */
-    status = WebRtcNs_Init(echo->NS_inst, echo->clock_rate);
-    if(status != 0) {
-        WEBRTC_NS_ERROR(echo->NS_inst, "re-initialization");
-    	return;
-    }
-
-    status = WebRtcNs_set_policy(echo->NS_inst, PJMEDIA_WEBRTC_NS_POLICY);
-    if (status != 0) {
-        WEBRTC_NS_ERROR(echo->NS_inst, "configuration re-initialization");
-        return;
-    }
-
-    /* re-initialize the EC */
+    /* re-initialize the AEC */
     status = WebRtcAec_Init(echo->AEC_inst, echo->clock_rate, echo->clock_rate);
     if(status != 0) {
         WEBRTC_AEC_ERROR(echo->AEC_inst, "re-initialization");
@@ -326,12 +440,49 @@ static void aec_reset(webrtc_ec *echo)
         return;
     }
 
+    /* re-initialize the AGC */
+    status = WebRtcAgc_Init(echo->AGC_inst, 0, 255, kAgcModeAdaptiveAnalog, echo->clock_rate);
+    if(status != 0) {
+        WEBRTC_AGC_ERROR(echo->AGC_inst, "initialization");
+    	return;
+    }
+
+    WebRtcAgc_config_t agc_config;
+    agc_config.targetLevelDbfs = 6;
+    agc_config.compressionGaindB = 9;
+    agc_config.limiterEnable = kAgcTrue;
+
+    status = WebRtcAgc_set_config(echo->AGC_inst, agc_config);
+    if(status != 0) {
+        WEBRTC_AGC_ERROR(echo->AGC_inst, "config initialization");
+    	return;
+    }
+
+    /* re-initialize the NS */
+    status = WebRtcNs_Init(echo->NS_inst, echo->clock_rate);
+    if(status != 0) {
+        WEBRTC_NS_ERROR(echo->NS_inst, "re-initialization");
+    	return;
+    }
+
+    status = WebRtcNs_set_policy(echo->NS_inst, PJMEDIA_WEBRTC_NS_POLICY);
+    if (status != 0) {
+        WEBRTC_NS_ERROR(echo->NS_inst, "configuration re-initialization");
+        return;
+    }
+
     /* cleanup temporary buffer */
     pj_bzero(echo->tmp_frame, 2*echo->samples_per_frame);
 
     /* re-initialize audio buffers */
     AudioBuffer_Initialize(&echo->capture_audio_buffer, echo->clock_rate);
     AudioBuffer_Initialize(&echo->playback_audio_buffer, echo->clock_rate);
+
+    /* re-initialize high pass filter state */
+    HighPassFilter_Initialize(&echo->hpf, echo->clock_rate);
+
+    /* re-initialize mic level */
+    echo->mic_capture_level = 255;
 
     PJ_LOG(4, (THIS_FILE, "WebRTC AEC reset succeeded"));
 }
@@ -350,7 +501,7 @@ PJ_DEF(pj_status_t) webrtc_aec_cancel_echo(void *state,
     int i, status;
 
     /* Sanity checks */
-    PJ_ASSERT_RETURN(echo && echo->AEC_inst && echo->NS_inst, PJ_EINVAL);
+    PJ_ASSERT_RETURN(echo && echo->AEC_inst && echo->AGC_inst && echo->NS_inst, PJ_EINVAL);
     PJ_ASSERT_RETURN(rec_frm && play_frm && options==0 && reserved==NULL, PJ_EINVAL);
 
     /* Check if a reset is needed */
@@ -367,6 +518,41 @@ PJ_DEF(pj_status_t) webrtc_aec_cancel_echo(void *state,
         AudioBuffer_SetData(&echo->capture_audio_buffer, (WebRtc_Word16 *) (&echo->tmp_frame[i]));
         AudioBuffer_SetData(&echo->playback_audio_buffer, (WebRtc_Word16 *) (&play_frm[i]));
 
+         /* Apply high pass filer */
+         HighPassFilter_Process(&echo->hpf,
+                                AudioBuffer_GetLowPassData(&echo->capture_audio_buffer),
+                                AudioBuffer_SamplesPerChannel(&echo->capture_audio_buffer));
+
+         /* Analyze capture data gain
+          * NOTE: if we used kAgcModeAdaptiveDigital we'd use WebRtcAgc_VirtualMic instead
+          */
+         status = WebRtcAgc_AddMic(echo->AGC_inst,
+                                   AudioBuffer_GetLowPassData(&echo->capture_audio_buffer),
+                                   AudioBuffer_GetHighPassData(&echo->capture_audio_buffer),
+                                   AudioBuffer_SamplesPerChannel(&echo->capture_audio_buffer));
+ 	if(status != 0) {
+             WEBRTC_AGC_ERROR(echo->AGC_inst, "gain analysis");
+ 	    return PJ_EBUG;
+ 	}
+
+        /* Feed farend buffer to AEC  */
+	status = WebRtcAec_BufferFarend(echo->AEC_inst,
+                                        AudioBuffer_GetLowPassData(&echo->playback_audio_buffer),
+	                                AudioBuffer_SamplesPerChannel(&echo->playback_audio_buffer));
+	if(status != 0) {
+            WEBRTC_AEC_ERROR(echo->AEC_inst, "farend buffering");
+	    return PJ_EBUG;
+	}
+
+        /* Feed farend buffer to AGC */
+	status = WebRtcAgc_AddFarend(echo->AGC_inst,
+                                     AudioBuffer_GetLowPassData(&echo->playback_audio_buffer),
+	                             AudioBuffer_SamplesPerChannel(&echo->playback_audio_buffer));
+	if(status != 0) {
+            WEBRTC_AGC_ERROR(echo->AGC_inst, "farend buffering");
+	    return PJ_EBUG;
+	}
+
         /* Noise suppression */
         status = WebRtcNs_Process(echo->NS_inst,
                                   AudioBuffer_GetLowPassData(&echo->capture_audio_buffer),
@@ -377,15 +563,6 @@ PJ_DEF(pj_status_t) webrtc_aec_cancel_echo(void *state,
             WEBRTC_NS_ERROR(echo->NS_inst, "ns processing");
             return PJ_EBUG;
         }
-
-        /* Feed farend buffer */
-	status = WebRtcAec_BufferFarend(echo->AEC_inst,
-                                        AudioBuffer_GetLowPassData(&echo->playback_audio_buffer),
-	                                AudioBuffer_SamplesPerChannel(&echo->playback_audio_buffer));
-	if(status != 0) {
-            WEBRTC_AEC_ERROR(echo->AEC_inst, "farend buffering");
-	    return PJ_EBUG;
-	}
 
 	/* Process echo cancellation */
         status = WebRtcAec_Process(echo->AEC_inst,
@@ -400,6 +577,34 @@ PJ_DEF(pj_status_t) webrtc_aec_cancel_echo(void *state,
             WEBRTC_AEC_ERROR(echo->AEC_inst, "echo processing");
 	    return PJ_EBUG;
         }
+
+        WebRtcAec_get_echo_status(echo->AEC_inst, &echo->has_echo);
+#if 0
+        if (echo->has_echo) {
+            PJ_LOG(4, (THIS_FILE, "Sound might have echo"));
+        }
+#endif
+
+        /* Process gain control */
+        status = WebRtcAgc_Process(echo->AGC_inst,
+                                   AudioBuffer_GetLowPassData(&echo->capture_audio_buffer),
+                                   AudioBuffer_GetHighPassData(&echo->capture_audio_buffer),
+                                   AudioBuffer_SamplesPerChannel(&echo->capture_audio_buffer),
+                                   AudioBuffer_GetLowPassData(&echo->capture_audio_buffer),
+                                   AudioBuffer_GetHighPassData(&echo->capture_audio_buffer),
+                                   echo->mic_capture_level,
+                                   &echo->mic_capture_level,
+                                   echo->has_echo,
+                                   &echo->is_saturated);
+        if (status != 0) {
+            WEBRTC_AGC_ERROR(echo->AGC_inst, "agc processing");
+            return PJ_EBUG;
+        }
+#if 0
+        if (echo->is_saturated) {
+            PJ_LOG(4, (THIS_FILE, "Sound might be saturated"));
+        }
+#endif
 
         /* finish frame processing, in case we are working at 32kHz low and high bands will be combined */
         AudioBuffer_GetData(&echo->capture_audio_buffer);
