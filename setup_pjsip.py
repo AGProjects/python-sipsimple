47f3b271239514a93c0b9e7f5ab37ee0fda890ce
Saul Ibarra <saul@ag-projects.com>
2014-05-22 09:30:25 +0000

    Silence unused-function warning caused by cython

--- setup_pjsip.py
+++ setup_pjsip.py
@@ -181,6 +181,7 @@ class PJSIP_build_ext(build_ext):
         extension.define_macros = [tuple(define.split("=", 1)) for define in self.get_opts_from_string(build_mak_vars["PJ_CFLAGS"], "-D")]
         extension.define_macros.append(("PJ_SVN_REVISION", open(os.path.join(self.build_dir, "base_rev"), "r").read().strip()))
         extension.define_macros.append(("__PYX_FORCE_INIT_THREADS", 1))
+        extension.extra_compile_args.append("-Wno-unused-function")    # silence warning
 
         if sys.platform == "darwin":
             extension.extra_link_args = list(itertools.chain(*[["-framework", val] for val in self.get_opts_from_string(build_mak_vars["PJ_LDLIBS"], "-framework ")]))
