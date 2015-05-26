115c964bca550d0c2c5d09063e63bc8b96561a6e
Saul Ibarra <saul@ag-projects.com>
2015-05-26 16:17:04 +0000

    pjsip: always enable video support

--- deps/pjsip/pjmedia/include/pjmedia/config.h
+++ deps/pjsip/pjmedia/include/pjmedia/config.h
@@ -1059,10 +1059,10 @@
 /**
  * Top level option to enable/disable video features.
  *
- * Default: 0 (disabled)
+ * Default: 1 (enabled)
  */
 #ifndef PJMEDIA_HAS_VIDEO
-#   define PJMEDIA_HAS_VIDEO				0
+#   define PJMEDIA_HAS_VIDEO				1
 #endif
 
 
--- setup_pjsip.py
+++ setup_pjsip.py
@@ -65,8 +65,6 @@ class PJSIP_build_ext(build_ext):
                    "#define PJSIP_TRANSPORT_IDLE_TIME 7200",
                    "#define PJ_ENABLE_EXTRA_CHECK 1",
                    "#define PJSIP_DONT_SWITCH_TO_TCP 1",
-                   "#define PJMEDIA_HAS_VIDEO 1",
-                   "#define PJMEDIA_HAS_FFMPEG 1",
                    "#define PJMEDIA_VIDEO_DEV_HAS_SDL 0",
                    "#define PJMEDIA_VIDEO_DEV_HAS_FB 1",
                    "#define PJMEDIA_VIDEO_DEV_HAS_FFMPEG 0",
