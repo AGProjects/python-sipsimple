0f076b42c1cc81de59b57238492281889c5031b7
Dan Pascu <dan@ag-projects.com>
2016-01-25 10:56:48 +0000

    Added OTR encryption support in the chat stream

--- sipsimple/streams/msrp/chat.py
+++ sipsimple/streams/msrp/chat.py
@@ -5,13 +5,17 @@
 This module provides classes to parse and generate SDP related to SIP sessions that negotiate Instant Messaging, including CPIM as defined in RFC3862
 """
 
-__all__ = ['ChatStream', 'ChatStreamError', 'ChatIdentity', 'CPIMPayload', 'CPIMHeader', 'CPIMNamespace', 'CPIMParserError']
-
+import cPickle as pickle
 import codecs
+import os
 import random
 import re
 
-from application.notification import NotificationCenter, NotificationData
+from application.python.descriptor import WriteOnceAttribute
+from application.notification import IObserver, NotificationCenter, NotificationData
+from application.python import Null
+from application.python.types import Singleton
+from application.system import openfile
 from collections import defaultdict
 from email.message import Message as EmailMessage
 from email.parser import Parser as EmailParser
@@ -20,15 +24,258 @@ from eventlib.proc import spawn, ProcExit
 from functools import partial
 from msrplib.protocol import FailureReportHeader, SuccessReportHeader, UseNicknameHeader
 from msrplib.session import MSRPSession, contains_mime_type
+from otr import OTRSession, OTRTransport, OTRState, SMPStatus
+from otr.cryptography import DSAPrivateKey
+from otr.exceptions import IgnoreMessage, UnencryptedMessage, EncryptedMessageError, OTRError
+from zope.interface import implements
 
 from sipsimple.core import SIPURI, BaseSIPURI
 from sipsimple.payloads import ParserError
 from sipsimple.payloads.iscomposing import IsComposingDocument, State, LastActive, Refresh, ContentType
+from sipsimple.storage import ISIPSimpleApplicationDataStorage
 from sipsimple.streams import InvalidStreamError, UnknownStreamError
 from sipsimple.streams.msrp import MSRPStreamError, MSRPStreamBase
-from sipsimple.threading import run_in_twisted_thread
+from sipsimple.threading import run_in_thread, run_in_twisted_thread
 from sipsimple.threading.green import run_in_green_thread
 from sipsimple.util import MultilingualText, ISOTimestamp
+
+
+__all__ = ['ChatStream', 'ChatStreamError', 'ChatIdentity', 'CPIMPayload', 'CPIMHeader', 'CPIMNamespace', 'CPIMParserError', 'OTRState', 'SMPStatus']
+
+
+class OTRTrustedPeer(object):
+    fingerprint = WriteOnceAttribute()  # in order to be hashable this needs to be immutable
+
+    def __init__(self, fingerprint, description=u'', **kw):
+        if not isinstance(fingerprint, basestring):
+            raise TypeError("fingerprint must be a string")
+        self.fingerprint = fingerprint
+        self.description = description
+        self.__dict__.update(kw)
+
+    def __hash__(self):
+        return hash(self.fingerprint)
+
+    def __eq__(self, other):
+        if isinstance(other, OTRTrustedPeer):
+            return self.fingerprint == other.fingerprint
+        elif isinstance(other, basestring):
+            return self.fingerprint == other
+        else:
+            return NotImplemented
+
+    def __ne__(self, other):
+        return not (self == other)
+
+    def __repr__(self):
+        return "{0.__class__.__name__}({0.fingerprint!r}, description={0.description!r})".format(self)
+
+    def __reduce__(self):
+        return self.__class__, (self.fingerprint,), self.__dict__
+
+
+class OTRTrustedPeerSet(object):
+    def __init__(self, iterable=()):
+        self.__data__ = {}
+        self.update(iterable)
+
+    def __repr__(self):
+        return "{}({})".format(self.__class__.__name__, self.__data__.values())
+
+    def __contains__(self, item):
+        return item in self.__data__
+
+    def __getitem__(self, item):
+        return self.__data__[item]
+
+    def __iter__(self):
+        return self.__data__.itervalues()
+
+    def __len__(self):
+        return len(self.__data__)
+
+    def get(self, item, default=None):
+        return self.__data__.get(item, default)
+
+    def add(self, item):
+        if not isinstance(item, OTRTrustedPeer):
+            raise TypeError("item should be and instance of OTRTrustedPeer")
+        self.__data__[item.fingerprint] = item
+
+    def remove(self, item):
+        del self.__data__[item]
+
+    def discard(self, item):
+        self.__data__.pop(item, None)
+
+    def update(self, iterable=()):
+        for item in iterable:
+            self.add(item)
+
+
+class OTRCache(object):
+    __metaclass__ = Singleton
+
+    def __init__(self):
+        from sipsimple.application import SIPApplication
+        if SIPApplication.storage is None:
+            raise RuntimeError("Cannot access the OTR cache before SIPApplication.storage is defined")
+        if ISIPSimpleApplicationDataStorage.providedBy(SIPApplication.storage):
+            self.key_file = os.path.join(SIPApplication.storage.directory, 'otr.key')
+            self.trusted_file = os.path.join(SIPApplication.storage.directory, 'otr.trusted')
+            try:
+                self.private_key = DSAPrivateKey.load(self.key_file)
+                if self.private_key.key_size != 1024:
+                    raise ValueError
+            except (EnvironmentError, ValueError):
+                self.private_key = DSAPrivateKey.generate()
+                self.private_key.save(self.key_file)
+            try:
+                self.trusted_peers = pickle.load(open(self.trusted_file, 'rb'))
+                if not isinstance(self.trusted_peers, OTRTrustedPeerSet) or not all(isinstance(item, OTRTrustedPeer) for item in self.trusted_peers):
+                    raise ValueError("invalid OTR trusted peers file")
+            except Exception:
+                self.trusted_peers = OTRTrustedPeerSet()
+                self.save()
+        else:
+            self.key_file = self.trusted_file = None
+            self.private_key = DSAPrivateKey.generate()
+            self.trusted_peers = OTRTrustedPeerSet()
+
+    # def generate_private_key(self):
+    #     self.private_key = DSAPrivateKey.generate()
+    #     if self.key_file:
+    #         self.private_key.save(self.key_file)
+
+    @run_in_thread('file-io')
+    def save(self):
+        if self.trusted_file is not None:
+            with openfile(self.trusted_file, 'wb', permissions=0600) as trusted_file:
+                pickle.dump(self.trusted_peers, trusted_file)
+
+
+class OTREncryption(object):
+    implements(IObserver)
+
+    def __init__(self, stream):
+        self.stream = stream
+        self.otr_cache = OTRCache()
+        self.otr_session = OTRSession(self.otr_cache.private_key, self.stream, supported_versions={3})  # we need at least OTR-v3 for question based SMP
+
+        notification_center = NotificationCenter()
+        notification_center.add_observer(self, sender=stream)
+        notification_center.add_observer(self, sender=self.otr_session)
+
+    @property
+    def active(self):
+        try:
+            return self.otr_session.encrypted
+        except AttributeError:
+            return False
+
+    @property
+    def key_fingerprint(self):
+        try:
+            return self.otr_session.local_private_key.public_key.fingerprint
+        except AttributeError:
+            return None
+
+    @property
+    def peer_fingerprint(self):
+        try:
+            return self.otr_session.remote_public_key.fingerprint
+        except AttributeError:
+            return None
+
+    @property
+    def peer_name(self):
+        try:
+            return self.__dict__['peer_name']
+        except KeyError:
+            trusted_peer = self.otr_cache.trusted_peers.get(self.peer_fingerprint, None)
+            return self.__dict__.setdefault('peer_name', trusted_peer.description if trusted_peer is not None else u'')
+
+    @peer_name.setter
+    def peer_name(self, name):
+        old_name = self.peer_name
+        new_name = self.__dict__['peer_name'] = name
+        if old_name != new_name:
+            trusted_peer = self.otr_cache.trusted_peers.get(self.peer_fingerprint, None)
+            if trusted_peer is not None:
+                trusted_peer.description = new_name
+                self.otr_cache.save()
+            notification_center = NotificationCenter()
+            notification_center.post_notification("ChatStreamOTRPeerNameChanged", sender=self.stream, data=NotificationData(name=name))
+
+    @property
+    def verified(self):
+        return self.peer_fingerprint in self.otr_cache.trusted_peers
+
+    @verified.setter
+    def verified(self, value):
+        peer_fingerprint = self.peer_fingerprint
+        old_verified = peer_fingerprint in self.otr_cache.trusted_peers
+        new_verified = bool(value)
+        if peer_fingerprint is None or new_verified == old_verified:
+            return
+        if new_verified:
+            self.otr_cache.trusted_peers.add(OTRTrustedPeer(peer_fingerprint, description=self.peer_name))
+        else:
+            self.otr_cache.trusted_peers.remove(peer_fingerprint)
+        self.otr_cache.save()
+        notification_center = NotificationCenter()
+        notification_center.post_notification("ChatStreamOTRVerifiedStateChanged", sender=self.stream, data=NotificationData(verified=new_verified))
+
+    @run_in_twisted_thread
+    def start(self):
+        if self.otr_session is not None:
+            self.otr_session.start()
+
+    @run_in_twisted_thread
+    def stop(self):
+        if self.otr_session is not None:
+            self.otr_session.stop()
+
+    @run_in_twisted_thread
+    def smp_verify(self, secret, question=None):
+        self.otr_session.smp_verify(secret, question)
+
+    @run_in_twisted_thread
+    def smp_answer(self, secret):
+        self.otr_session.smp_answer(secret)
+
+    @run_in_twisted_thread
+    def smp_abort(self):
+        self.otr_session.smp_abort()
+
+    def handle_notification(self, notification):
+        handler = getattr(self, '_NH_%s' % notification.name, Null)
+        handler(notification)
+
+    def _NH_MediaStreamDidStart(self, notification):
+        if self.stream.start_otr:
+            self.otr_session.start()
+
+    def _NH_MediaStreamDidEnd(self, notification):
+        notification.center.remove_observer(self, sender=self.stream)
+        notification.center.remove_observer(self, sender=self.otr_session)
+        self.otr_session.stop()
+        self.otr_session = None
+        self.stream = None
+
+    _NH_MediaStreamDidNotInitialize = _NH_MediaStreamDidEnd
+
+    def _NH_OTRSessionStateChanged(self, notification):
+        notification.center.post_notification('ChatStreamOTREncryptionStateChanged', sender=self.stream, data=notification.data)
+
+    def _NH_OTRSessionSMPVerificationDidStart(self, notification):
+        notification.center.post_notification('ChatStreamSMPVerificationDidStart', sender=self.stream, data=notification.data)
+
+    def _NH_OTRSessionSMPVerificationDidNotStart(self, notification):
+        notification.center.post_notification('ChatStreamSMPVerificationDidNotStart', sender=self.stream, data=notification.data)
+
+    def _NH_OTRSessionSMPVerificationDidEnd(self, notification):
+        notification.center.post_notification('ChatStreamSMPVerificationDidEnd', sender=self.stream, data=notification.data)
 
 
 class ChatStreamError(MSRPStreamError): pass
@@ -44,6 +291,7 @@ class ChatStream(MSRPStreamBase):
     accept_wrapped_types = ['text/*', 'image/*', 'application/im-iscomposing+xml']
 
     prefer_cpim = True
+    start_otr = True
 
     def __init__(self):
         super(ChatStream, self).__init__(direction='sendrecv')
@@ -51,6 +299,7 @@ class ChatStream(MSRPStreamBase):
         self.sent_messages = set()
         self.incoming_queue = defaultdict(list)
         self.message_queue_thread = None
+        self.encryption = OTREncryption(self)
 
     @classmethod
     def new_from_sdp(cls, session, remote_sdp, stream_index):
@@ -155,6 +404,7 @@ class ChatStream(MSRPStreamBase):
             return
         else:
             data = ''.join(self.incoming_queue.pop(chunk.message_id, [])) + chunk.data
+
         if content_type == 'message/cpim':
             try:
                 payload = CPIMPayload.decode(data)
@@ -176,6 +426,25 @@ class ChatStream(MSRPStreamBase):
             message = Message(payload.content, payload.content_type, sender=self.remote_identity, recipients=[self.local_identity], timestamp=ISOTimestamp.now())
             private = False
 
+        try:
+            message.content = self.encryption.otr_session.handle_input(message.content, message.content_type)
+        except IgnoreMessage:
+            self.msrp_session.send_report(chunk, 200, 'OK')
+            return
+        except UnencryptedMessage:
+            encrypted = False
+            encryption_active = True
+        except EncryptedMessageError, e:
+            self.msrp_session.send_report(chunk, 400, str(e))
+            return
+        except OTRError, e:
+            self.msrp_session.send_report(chunk, 200, 'OK')
+            notification_center = NotificationCenter()
+            notification_center.post_notification('ChatStreamGotOTRError', sender=self, data=NotificationData(error=str(e)))
+            return
+        else:
+            encrypted = encryption_active = self.encryption.active
+
         if payload.charset is not None:
             message.content = message.content.decode(payload.charset)
         elif payload.content_type.startswith('text/'):
@@ -193,11 +462,12 @@ class ChatStream(MSRPStreamBase):
                                     refresh=document.refresh.value if document.refresh is not None else 120,
                                     content_type=document.content_type.value if document.content_type is not None else None,
                                     last_active=document.last_active.value if document.last_active is not None else None,
-                                    sender=message.sender, recipients=message.recipients, private=private)
+                                    sender=message.sender, recipients=message.recipients, private=private,
+                                    encrypted=encrypted, encryption_active=encryption_active)
             notification_center.post_notification('ChatStreamGotComposingIndication', sender=self, data=data)
         else:
             self.msrp_session.send_report(chunk, 200, 'OK')
-            data = NotificationData(message=message, private=private)
+            data = NotificationData(message=message, private=private, encrypted=encrypted, encryption_active=encryption_active)
             notification_center.post_notification('ChatStreamGotMessage', sender=self, data=data)
 
     def _on_transaction_response(self, message_id, response):
@@ -223,12 +493,19 @@ class ChatStream(MSRPStreamBase):
                         data = NotificationData(message_id=message.id, message=None, code=0, reason='Stream ended')
                         notification_center.post_notification('ChatStreamDidNotDeliverMessage', sender=self, data=data)
                     break
+
                 try:
                     if isinstance(message.content, unicode):
                         message.content = message.content.encode('utf8')
                         charset = 'utf8'
                     else:
                         charset = None
+
+                    if not isinstance(message, QueuedOTRInternalMessage):
+                        try:
+                            message.content = self.encryption.otr_session.handle_output(message.content, message.content_type)
+                        except OTRError, e:
+                            raise ChatStreamError(str(e))
 
                     message.sender = message.sender or self.local_identity
                     message.recipients = message.recipients or [self.remote_identity]
@@ -319,6 +596,10 @@ class ChatStream(MSRPStreamBase):
             self._failure_reason = str(e)
             NotificationCenter().post_notification('MediaStreamDidFail', sender=self, data=NotificationData(context='sending', reason=self._failure_reason))
 
+    def inject_otr_message(self, data):
+        message = QueuedOTRInternalMessage(data)
+        self._enqueue_message(message)
+
     def send_message(self, content, content_type='text/plain', recipients=None, courtesy_recipients=None, subject=None, timestamp=None, required=None, additional_headers=None):
         message = QueuedMessage(content, content_type, recipients=recipients, courtesy_recipients=courtesy_recipients, subject=subject, timestamp=timestamp, required=required, additional_headers=additional_headers, notify_progress=True)
         self._enqueue_message(message)
@@ -336,6 +617,9 @@ class ChatStream(MSRPStreamBase):
         message_id = '%x' % random.getrandbits(64)
         self._set_local_nickname(nickname, message_id)
         return message_id
+
+
+OTRTransport.register(ChatStream)
 
 
 # Chat related objects, including CPIM support as defined in RFC3862
@@ -408,6 +692,11 @@ class QueuedMessage(Message):
         super(QueuedMessage, self).__init__(content, content_type, sender, recipients, courtesy_recipients, subject, timestamp, required, additional_headers)
         self.id = id or '%x' % random.getrandbits(64)
         self.notify_progress = notify_progress
+
+
+class QueuedOTRInternalMessage(QueuedMessage):
+    def __init__(self, content):
+        super(QueuedOTRInternalMessage, self).__init__(content, 'text/plain', notify_progress=False)
 
 
 class SimplePayload(object):
