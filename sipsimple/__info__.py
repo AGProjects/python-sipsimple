b79db43ab61ba265c25919108b6fc5b5237c6e24
Dan Pascu <dan@ag-projects.com>
2018-12-12 05:03:33 +0000

    Increased version and updated changelogs

--- sipsimple/__info__.py
+++ sipsimple/__info__.py
@@ -5,7 +5,7 @@ __project__ = "python-sipsimple"
 __summary__ = "SIP SIMPLE implementation for python"
 __webpage__ = "http://sipsimpleclient.org"
 
-__version__ = "3.2.1"
+__version__ = "3.3.0"
 
 __author__ = "AG Projects"
 __email__ = "support@ag-projects.com"
