f44c577e2428a8a40e3ab6d0233a0806b0d66c40
Dan Pascu <dan@ag-projects.com>
2019-02-25 10:42:56 +0000

    Updated copyright years

--- sipsimple/__info__.py
+++ sipsimple/__info__.py
@@ -11,4 +11,4 @@ __author__ = "AG Projects"
 __email__ = "support@ag-projects.com"
 
 __license__ = "GPL"
-__copyright__ = "Copyright 2008-2018 {}".format(__author__)
+__copyright__ = "Copyright 2008-2019 {}".format(__author__)
