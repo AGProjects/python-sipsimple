8c3d0c7866840b719623983176a7a4b8b6c97924
Adrian Georgescu <ag@ag-projects.com>
2021-01-10 13:56:32 +0000

    Increase version to 3.5.1

--- sipsimple/__info__.py
+++ sipsimple/__info__.py
@@ -2,13 +2,13 @@
 """Package information"""
 
 __project__ = "python-sipsimple"
-__summary__ = "SIP SIMPLE implementation for python"
-__webpage__ = "http://sipsimpleclient.org"
+__summary__ = "SIP SIMPLE implementation for Python"
+__webpage__ = "https://sipsimpleclient.org"
 
-__version__ = "3.5.0"
+__version__ = "3.5.1"
 
 __author__ = "AG Projects"
 __email__ = "support@ag-projects.com"
 
 __license__ = "GPL"
-__copyright__ = "Copyright 2008-2020 {}".format(__author__)
+__copyright__ = "Copyright 2008-2021 {}".format(__author__)
