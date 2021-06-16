a644f25948de1f9b190844d04384159f20738c1e
Tijmen de Mes <tijmen@ag-projects.com>
2021-06-16 13:06:13 +0000

    Updated changelog and raised version

--- sipsimple/__info__.py
+++ sipsimple/__info__.py
@@ -5,7 +5,7 @@ __project__ = "python-sipsimple"
 __summary__ = "SIP SIMPLE implementation for Python"
 __webpage__ = "https://sipsimpleclient.org"
 
-__version__ = "3.5.1"
+__version__ = "3.6.0"
 
 __author__ = "AG Projects"
 __email__ = "support@ag-projects.com"
